d7731a54dcab57f7b830bcbc9b5ab66cddabd915
A S Cross <andrew-stuart-cross@outlook.com>
2019-03-01 19:27:10 +0000

    observations-feed added to routing module

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { BirdsListComponent } from './birds-list/birds-list.component';
+import { ObservationsFeedComponent } from './observations-feed/observations-feed.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'counter', component: CounterComponent },
   { path: 'fetch-data', component: FetchDataComponent },
-  { path: 'birds-list', component: BirdsListComponent }
+  { path: 'birds-list', component: BirdsListComponent },
+  { path: 'observations-feed', component: ObservationsFeedComponent}
 ];
 
 @NgModule({
