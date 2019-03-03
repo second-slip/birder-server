56f7e45e3ad66e8965006cbaa3946efe50ab505c
A S Cross <andrew-stuart-cross@outlook.com>
2019-03-03 17:38:39 +0000

    validators added

---
+++ ClientApp/src/validators/index.ts
@@ -0,0 +1,4 @@
+export { Country } from './country.model';
+export { PasswordValidator, ParentErrorStateMatcher } from './password.validator';
+export { PhoneValidator } from './phone.validator';
+export { UsernameValidator } from './username.validator';
