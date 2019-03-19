e530284ea00666deb88864f46671637ee1766d3d
A S Cross <andrew-stuart-cross@outlook.com>
2019-03-19 14:56:03 +0000

    Google map added to edit observation component

--- ClientApp/src/app/observation-edit/observation-edit.component.ts
+++ ClientApp/src/app/observation-edit/observation-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ChangeDetectorRef } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { ParentErrorStateMatcher } from '../../validators';
 import { ObservationService } from '../observation.service';
@@ -7,6 +7,8 @@ import { ErrorReportViewModel } from '../../_models/ErrorReportViewModel';
 import { ObservationViewModel } from '../../_models/ObservationViewModel';
 import { BirdSummaryViewModel } from '../../_models/BirdSummaryViewModel';
 import { BirdsService } from '../birds.service';
+import { GeocodeService } from '../geocode.service';
+import { LocationViewModel } from '../../_models/LocationViewModel';
 
 @Component({
   selector: 'app-observation-edit',
@@ -21,6 +23,12 @@ export class ObservationEditComponent implements OnInit {
   parentErrorStateMatcher = new ParentErrorStateMatcher();
   errorReport: ErrorReportViewModel;
 
+    //
+    geolocation: string;
+    searchAddress = '';
+    geoError: string;
+    //
+
   editObservation_validation_messages = {
     'quantity': [
       { type: 'required', message: 'Quantity is required' }
@@ -34,7 +42,9 @@ export class ObservationEditComponent implements OnInit {
             , private route: ActivatedRoute
             , private observationService: ObservationService
             , private birdsService: BirdsService
-            , private formBuilder: FormBuilder) { }
+            , private formBuilder: FormBuilder
+            , private geocodeService: GeocodeService
+            , private ref: ChangeDetectorRef) { }
 
   ngOnInit() {
     this.getObservation();
@@ -54,6 +64,8 @@ export class ObservationEditComponent implements OnInit {
       observationDateTime: new FormControl(this.observation.observationDateTime, Validators.compose([
         Validators.required
       ])),
+      locationLatitude: new FormControl(this.observation.locationLatitude),
+      locationLongitude: new FormControl(this.observation.locationLongitude),
       noteGeneral: new FormControl(this.observation.noteGeneral),
       noteHabitat: new FormControl(this.observation.noteHabitat),
       noteWeather: new FormControl(this.observation.noteWeather),
@@ -91,13 +103,12 @@ export class ObservationEditComponent implements OnInit {
       (observation: ObservationViewModel) => {
         this.observation = observation;
         this.createForms();
+        this.getGeolocation();
       },
       (error: ErrorReportViewModel) => {
         this.errorReport = error;
         // this.router.navigate(['/page-not-found']);  // TODO: this is right for typing bad param, but what about server error?
       });
-
-
   }
 
   getBirds(): void {
@@ -110,4 +121,73 @@ export class ObservationEditComponent implements OnInit {
       });
   }
 
+  // new google maps methods...
+
+  getGeolocation(): void {
+    this.geocodeService.reverseGeocode(this.observation.locationLatitude, this.observation.locationLongitude)
+      .subscribe(
+        (data: LocationViewModel) => {
+          this.geolocation = data.formattedAddress;
+          this.ref.detectChanges();
+        },
+        (error: any) => {
+          //
+        }
+      );
+  }
+
+  useGeolocation(searchValue: string) {
+    this.geocodeService.geocodeAddress(searchValue)
+      .subscribe((location: LocationViewModel) => {
+        this.editObservationForm.get('locationLatitude').setValue(location.latitude);
+        this.editObservationForm.get('locationLongitude').setValue(location.longitude);
+        this.geolocation = location.formattedAddress;
+        this.searchAddress = '';
+        this.ref.detectChanges();
+      }
+      );
+  }
+
+  closeAlert() {
+    this.geoError = null;
+  }
+
+  getCurrentPosition() {
+    if (window.navigator.geolocation) {
+      window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+          this.useGeolocation(position.coords.latitude.toString() + ',' + position.coords.longitude.toString());
+        }, (error) => {
+          switch (error.code) {
+            case 3: // ...deal with timeout
+              this.geoError = 'The request to get user location timed out...';
+              break;
+            case 2: // ...device can't get data
+              this.geoError = 'Location information is unavailable...';
+              break;
+            case 1: // ...user said no ☹️
+              this.geoError = 'User denied the request for Geolocation...';
+              break;
+            default:
+              this.geoError = 'An error occurred with Geolocation...';
+          }
+        });
+    } else {
+      this.geoError = 'Geolocation not supported in this browser';
+    }
+  }
+
+  placeMarker($event) {
+    this.geocodeService.reverseGeocode($event.coords.lat, $event.coords.lng)
+      .subscribe(
+        (location: LocationViewModel) => {
+          this.editObservationForm.get('locationLatitude').setValue(location.latitude);
+          this.editObservationForm.get('locationLongitude').setValue(location.longitude);
+          this.geolocation = location.formattedAddress;
+          this.ref.detectChanges();
+        },
+        (error: any) => { }
+      );
+  }
+  // new google maps methods...
 }
