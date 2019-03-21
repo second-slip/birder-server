a00fb5657077a8aa9be3db2ef13234b4d3e73d1e
A S Cross <andrew-stuart-cross@outlook.com>
2019-03-21 16:07:15 +0000

    ObservationAnalysisController added

---
+++ Controllers/ObservationAnalysisController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Birder.Data;
+using Birder.Data.Model;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Birder.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = "Bearer")]
+    public class ObservationAnalysisController : ControllerBase
+    {
+
+
+        public ObservationAnalysisController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Observation>>> GetObservations()
+        {
+            return await _context.Observations.ToListAsync();
+        }       
+    }
+}
