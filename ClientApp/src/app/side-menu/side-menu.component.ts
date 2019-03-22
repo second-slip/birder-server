dab5fb0a3985055e24eabbd3dc41cc022c84345c
A S Cross <andrew-stuart-cross@outlook.com>
2019-03-22 14:22:19 +0000

    scss

--- ClientApp/src/app/side-menu/side-menu.component.ts
+++ ClientApp/src/app/side-menu/side-menu.component.ts
@@ -4,7 +4,7 @@ import { ObservationsAnalysisService } from '../observations-analysis.service';
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
-  styleUrls: ['./side-menu.component.css']
+  styleUrls: ['./side-menu.component.scss']
 })
 export class SideMenuComponent implements OnInit {
 
