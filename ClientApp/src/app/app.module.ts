331aff977779aea839f57825c18fdf8e9b9978d3
A S Cross <andrew-stuart-cross@outlook.com>
2019-04-03 23:18:40 +0100

    top observered in table format

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -13,7 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SideMenuComponent } from './side-menu/side-menu.component';
 import { MatIconModule, MatButtonModule, MatInputModule,
               MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSelectModule,
-              MatProgressSpinnerModule, MAT_DATE_LOCALE, MatTooltipModule, MatChipsModule, MatAutocompleteModule, MatExpansionModule} from '@angular/material';
+              MatProgressSpinnerModule, MAT_DATE_LOCALE, MatTooltipModule, MatChipsModule,
+              MatAutocompleteModule, MatExpansionModule, MatTabsModule} from '@angular/material';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './auth-guard.service';
 import { LogoutComponent } from './logout/logout.component';
@@ -82,6 +83,7 @@ export function tokenGetter() {
     MatChipsModule,
     MatAutocompleteModule,
     MatExpansionModule,
+    MatTabsModule,
     JwtModule.forRoot({
       config: {
         tokenGetter: tokenGetter,
