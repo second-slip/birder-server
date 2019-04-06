2893795403622c4673947d79e194350446615120
A S Cross <andrew-stuart-cross@outlook.com>
2019-04-06 20:02:13 +0100

    Two layouts implementation

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -16,23 +16,39 @@ import { ConfirmEmailComponent } from './confirm-email/confirm-email.component';
 import { ObservationEditComponent } from './observation-edit/observation-edit.component';
 import { ObservationDeleteComponent } from './observation-delete/observation-delete.component';
 import { LifeListComponent } from './life-list/life-list.component';
+import { LayoutNoSidebarComponent } from './layout-no-sidebar/layout-no-sidebar.component';
+import { LayoutSidebarComponent } from './layout-sidebar/layout-sidebar.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'login', component: LoginComponent},
-  { path: 'register', component: RegisterComponent},
-  { path: 'logout', component: LogoutComponent},
-  { path: 'counter', component: CounterComponent },  //
-  { path: 'observation-feed', component: ObservationFeedComponent, canActivate: [AuthGuard] },
-  { path: 'observation-add', component: ObservationAddComponent, canActivate: [AuthGuard] },
-  { path: 'observation-detail/:id', component: ObservationDetailComponent, canActivate: [AuthGuard] },
-  { path: 'observation-edit/:id', component: ObservationEditComponent, canActivate: [AuthGuard] },
-  { path: 'observation-delete/:id', component: ObservationDeleteComponent, canActivate: [AuthGuard] },
-  { path: 'birds-index', component: BirdsIndexComponent, canActivate: [AuthGuard] },
-  { path: 'birds-detail/:id', component: BirdsDetailComponent, canActivate: [AuthGuard] },
-  { path: 'life-list', component: LifeListComponent, canActivate: [AuthGuard] },
-  { path: 'confirm-email', component: ConfirmEmailComponent },
-  { path: '**', component: PageNotFoundComponent }
+
+  {
+    path: '',
+    component: LayoutNoSidebarComponent,
+    children: [
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'login', component: LoginComponent},
+      { path: 'register', component: RegisterComponent},
+      { path: 'logout', component: LogoutComponent},
+      { path: 'counter', component: CounterComponent },  //
+    ]
+  },
+  {
+    path: '',
+    component: LayoutSidebarComponent,
+    children: [
+      { path: 'observation-feed', component: ObservationFeedComponent, canActivate: [AuthGuard] },
+      { path: 'observation-add', component: ObservationAddComponent, canActivate: [AuthGuard] },
+      { path: 'observation-detail/:id', component: ObservationDetailComponent, canActivate: [AuthGuard] },
+      { path: 'observation-edit/:id', component: ObservationEditComponent, canActivate: [AuthGuard] },
+      { path: 'observation-delete/:id', component: ObservationDeleteComponent, canActivate: [AuthGuard] },
+      { path: 'birds-index', component: BirdsIndexComponent, canActivate: [AuthGuard] },
+      { path: 'birds-detail/:id', component: BirdsDetailComponent, canActivate: [AuthGuard] },
+      { path: 'life-list', component: LifeListComponent, canActivate: [AuthGuard] },
+      { path: 'confirm-email', component: ConfirmEmailComponent },
+    ]
+  },
+
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
