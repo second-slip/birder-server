52e5fc441401bd2ab1e00178c212c5d1ae173b08
A S Cross <andrew-stuart-cross@outlook.com>
2019-04-07 06:42:12 +0100

    Code tidying

--- ClientApp/src/app/auth-guard.service.ts
+++ ClientApp/src/app/auth-guard.service.ts
@@ -9,45 +9,23 @@ export class AuthGuard implements CanActivate, CanActivateChild {
             , private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-
-    // TODO: use authenticationService?
     const token = localStorage.getItem('jwt');
 
     if (token && !this.jwtHelper.isTokenExpired(token)) {
       // console.log(this.jwtHelper.decodeToken(token));
-      // console.log('valid token');
       return true;
     }
-    // if (token && this.jwtHelper.isTokenExpired(token)) {
-    //   // console.log(this.jwtHelper.decodeToken(token));
-    //   console.log('expired token');
-    //   this.router.navigate(['/login']);
-    //   return false;
-    // }
-    // console.log('NO token');
-    // this.router.navigate(['/login']);
     this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
     return false;
   }
 
   canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-
-    // TODO: use authenticationService?
     const token = localStorage.getItem('jwt');
 
     if (token && !this.jwtHelper.isTokenExpired(token)) {
       // console.log(this.jwtHelper.decodeToken(token));
-      console.log('valid token');
       return true;
     }
-    // if (token && this.jwtHelper.isTokenExpired(token)) {
-    //   // console.log(this.jwtHelper.decodeToken(token));
-    //   console.log('expired token');
-    //   this.router.navigate(['login']);
-    //   return false;
-    // }
-    // console.log('NO token');
-    // this.router.navigate(['login']);
     this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
     return false;
   }
