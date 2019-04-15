03a6120045cb47e36033ce41243aba5629f9a0cc
A S Cross <andrew-stuart-cross@outlook.com>
2019-04-15 22:12:16 +0100

    Work on info-network

--- ClientApp/src/app/_info/info-network/info-network.component.ts
+++ ClientApp/src/app/_info/info-network/info-network.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { UserProfileViewModel } from '../../../_models/UserProfileViewModel';
 
 @Component({
   selector: 'app-info-network',
   templateUrl: './info-network.component.html',
-  styleUrls: ['./info-network.component.scss']
+  styleUrls: ['./info-network.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class InfoNetworkComponent implements OnInit {
+  user: UserProfileViewModel;
 
   constructor() { }
 
