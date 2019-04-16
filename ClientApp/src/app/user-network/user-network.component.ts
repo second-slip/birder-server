fd39ca25317887fcbbb800611f45bc01b671a8bf
A S Cross <andrew-stuart-cross@outlook.com>
2019-04-16 12:19:30 +0100

    Further work on user network page

--- ClientApp/src/app/user-network/user-network.component.ts
+++ ClientApp/src/app/user-network/user-network.component.ts
@@ -14,6 +14,7 @@ import { Router } from '@angular/router';
 export class UserNetworkComponent implements OnInit {
   users: NetworkUserViewModel[];
   searchTerm: string;
+  customSearch: boolean;
 
   constructor(private userService: UserService
     , private router: Router
@@ -25,6 +26,7 @@ export class UserNetworkComponent implements OnInit {
 
   search(): void {
     this.getNetwork(this.searchTerm);
+    this.customSearch = true;
   }
 
   getNetwork(searchTerm: string): void {
