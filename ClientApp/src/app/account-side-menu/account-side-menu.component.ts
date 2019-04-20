9a069bb164df892f293bc1862bd5ec08b81c56f9
A S Cross <andrew-stuart-cross@outlook.com>
2019-04-20 15:15:53 +0100

    Account-side-menu css styling

--- ClientApp/src/app/account-side-menu/account-side-menu.component.ts
+++ ClientApp/src/app/account-side-menu/account-side-menu.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-account-side-menu',
   templateUrl: './account-side-menu.component.html',
-  styleUrls: ['./account-side-menu.component.scss']
+  styleUrls: ['./account-side-menu.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AccountSideMenuComponent implements OnInit {
 
