ffe21ee24ddf793d80d1fcf52824a393963bbf07
A S Cross <andrew-stuart-cross@outlook.com>
2019-04-20 12:58:52 +0100

    model validation attributes added

--- ViewModels/ManageProfileViewModel.cs
+++ ViewModels/ManageProfileViewModel.cs
@@ -6,6 +6,7 @@ namespace Birder.ViewModels
     public class ManageProfileViewModel
     {
         [Required]
+        [StringLength(25, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 5)]
         public string UserName { get; set; }
 
         public bool IsEmailConfirmed { get; set; }
