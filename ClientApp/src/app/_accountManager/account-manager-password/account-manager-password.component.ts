fa81b7057ede24a006171a76486f5bc9f25bcaee
A S Cross <andrew-stuart-cross@outlook.com>
2019-04-20 18:52:50 +0100

    Workon Change Password form

--- ClientApp/src/app/_accountManager/account-manager-password/account-manager-password.component.ts
+++ ClientApp/src/app/_accountManager/account-manager-password/account-manager-password.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-account-manager-password',
   templateUrl: './account-manager-password.component.html',
-  styleUrls: ['./account-manager-password.component.scss']
+  styleUrls: ['./account-manager-password.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AccountManagerPasswordComponent implements OnInit {
 
