caefddd29742150d544a00b1c6242cc17365fb1d
A S Cross <andrew-stuart-cross@outlook.com>
2019-04-27 20:13:35 +0100

    ModelStateErrosHelper class added

---
+++ Helpers/ModelStateErrorsHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Linq;
+
+namespace Birder.Helpers
+{
+    public static class ModelStateErrorsExtensions
+    {
+        //ToDo: change to an extension method?
+        public static string GetModelStateErrorMessages(ModelStateDictionary modelState)
+        {
+            string validationErrors = string.Join("; ",
+                    modelState.Values.Where(E => E.Errors.Count > 0)
+                        .SelectMany(E => E.Errors)
+                        .Select(E => E.ErrorMessage)
+                        .ToArray());
+
+            return validationErrors;
+        }
+    }
+}
