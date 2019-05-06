3dc3de60bbc081f0888ef25e53d03f0a380bbeef
A S Cross <andrew-stuart-cross@outlook.com>
2019-05-06 13:59:15 +0100

    db experiments

--- Controllers/ObservationAnalysisController.cs
+++ Controllers/ObservationAnalysisController.cs
@@ -79,21 +79,19 @@ namespace Birder.Controllers
                     return Unauthorized();
                 }
 
-                _cache.Remove(nameof(TopObservationsAnalysisViewModel));
-
                 if (_cache.TryGetValue(nameof(TopObservationsAnalysisViewModel), out TopObservationsAnalysisViewModel topObservationsCache))
                 {
                     return Ok(topObservationsCache);
                 }
 
-                //var viewModel = await _observationsAnalysisRepository.gtAsync(username, _systemClock.GetToday.AddDays(-30));
+                var viewModel = await _observationsAnalysisRepository.gtAsync(username, _systemClock.GetToday.AddDays(-30));
                 //viewModel.TopObservations = _observationsAnalysisRepository.GetTopObservations(username);
 
-                var viewModel = new TopObservationsAnalysisViewModel
-                {
-                    TopObservations = _observationsAnalysisRepository.GetTopObservations(username),
-                    TopMonthlyObservations = _observationsAnalysisRepository.GetTopObservations(username, _systemClock.GetToday.AddDays(-30))
-                };
+                // var viewModel = new TopObservationsAnalysisViewModel
+                // {
+                //     TopObservations = _observationsAnalysisRepository.GetTopObservations(username),
+                //     TopMonthlyObservations = _observationsAnalysisRepository.GetTopObservations(username, _systemClock.GetToday.AddDays(-30))
+                // };
 
                 var cacheEntryExpiryDate = TimeSpan.FromDays(1);
 
