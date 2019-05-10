fce5d3f07908b320bb66bac4cc0495cc7b1e7ad6
A S Cross <andrew-stuart-cross@outlook.com>
2019-05-10 17:37:10 +0100

    LifeListMappingHelper added

--- Controllers/ObservationAnalysisController.cs
+++ Controllers/ObservationAnalysisController.cs
@@ -160,18 +160,19 @@ namespace Birder.Controllers
 
                 _cache.Set(CacheEntries.ObservationsList, observations, _systemClock.GetEndOfToday);
 
-                var viewModel = observations
-                    .GroupBy(n => n.Bird)
-                    .Select(n => new LifeListViewModel
-                            {
-                                BirdId = n.Key.BirdId,
-                                EnglishName = n.Key.EnglishName,
-                                Species = n.Key.Species,
-                                PopulationSize = n.Key.PopulationSize,
-                                BtoStatusInBritain = n.Key.BtoStatusInBritain,
-                                ConservationStatus = n.Key.BirdConservationStatus.ConservationList,
-                                Count = n.Count()
-                            }).OrderByDescending(n => n.Count);
+                //var viewModel = observations
+                //    .GroupBy(n => n.Bird)
+                //    .Select(n => new LifeListViewModel
+                //            {
+                //                BirdId = n.Key.BirdId,
+                //                EnglishName = n.Key.EnglishName,
+                //                Species = n.Key.Species,
+                //                PopulationSize = n.Key.PopulationSize,
+                //                BtoStatusInBritain = n.Key.BtoStatusInBritain,
+                //                ConservationStatus = n.Key.BirdConservationStatus.ConservationList,
+                //                Count = n.Count()
+                //            }).OrderByDescending(n => n.Count);
+                var viewModel = LifeListMappingHelper.MapLifeList(observations);
 
                 return Ok(viewModel);
             }
