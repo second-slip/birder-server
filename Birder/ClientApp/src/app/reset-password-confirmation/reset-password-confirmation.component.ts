445c276c3c890191f6dc771a0375ac34c96924b0
A S Cross <andrew-stuart-cross@outlook.com>
2019-05-15 12:20:47 +0100

    reset password confirmation finished

--- Birder/ClientApp/src/app/reset-password-confirmation/reset-password-confirmation.component.ts
+++ Birder/ClientApp/src/app/reset-password-confirmation/reset-password-confirmation.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-reset-password-confirmation',
   templateUrl: './reset-password-confirmation.component.html',
-  styleUrls: ['./reset-password-confirmation.component.scss']
+  styleUrls: ['./reset-password-confirmation.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ResetPasswordConfirmationComponent implements OnInit {
 
