ed74d0f8f13fa4c2056a61a19221d8255e1f4828
A S Cross <andrew-stuart-cross@outlook.com>
2019-05-15 12:32:29 +0100

    UrlService

--- Birder/Services/UrlService.cs
+++ Birder/Services/UrlService.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Threading.Tasks;
 using System.Web;
 
@@ -10,6 +11,7 @@ namespace Birder.Services
     public interface IUrlService
     {
         Uri ResetPasswordUrl(string code);
+        //Uri ConfirmEmailUrl(string username, string code);
     }
     public class UrlService : IUrlService
     {
