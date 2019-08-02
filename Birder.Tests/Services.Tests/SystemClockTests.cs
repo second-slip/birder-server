c8c235aabfd4cb2364a21165dab4e8146dfc2415
A S Cross <andrew-stuart-cross@outlook.com>
2019-08-02 14:14:41 +0100

    SystemClock unit tests added

---
+++ Birder.Tests/Services.Tests/SystemClockTests.cs
@@ -0,0 +1,51 @@
+﻿using Birder.Services;
+using System;
+using Xunit;
+
+namespace Birder.Tests.Services.Tests
+{
+    public class SystemClockTests
+    {
+        [Fact]
+        public void GetNowTest()
+        {
+            //Arrange
+            var service = new SystemClock();
+            var expected = DateTime.Now;
+
+            //Act
+            var actual = service.GetNow;
+
+            //Assert
+            Assert.Equal(expected, actual, TimeSpan.FromSeconds(1));
+        }
+
+        [Fact]
+        public void GetTodayTest()
+        {
+            //Arrange
+            var service = new SystemClock();
+            var expected = DateTime.Today;
+
+            //Act
+            var actual = service.GetToday;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetEndOfTodayTest()
+        {
+            //Arrange
+            var service = new SystemClock();
+            var expected = DateTime.Today.Date.AddDays(1).AddTicks(-1);
+
+            //Act
+            var actual = service.GetEndOfToday;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
--- Birder/ClientApp/src/app/geocode.service.ts
+++ Birder/ClientApp/src/app/geocode.service.ts
@@ -82,5 +82,4 @@ export class GeocodeService {
       })
     );
   }
-
 }
