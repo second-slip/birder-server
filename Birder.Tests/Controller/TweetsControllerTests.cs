7566dd50b1ba5c11f0db9a0337bac6586ed7399d
A S Cross <andrew-stuart-cross@outlook.com>
2019-08-04 18:14:49 +0100

    Tweet controller logic test passes

--- Birder.Tests/Controller/TweetsControllerTests.cs
+++ Birder.Tests/Controller/TweetsControllerTests.cs
@@ -3,11 +3,13 @@ using Birder.Controllers;
 using Birder.Data.Model;
 using Birder.Data.Repository;
 using Birder.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -19,47 +21,59 @@ namespace Birder.Tests.Controller
         private readonly Mock<IMapper> _mapper;
         private readonly Mock<ILogger<TweetsController>> _logger;
         private readonly Mock<ISystemClockService> _systemClock;
-        //private readonly Mock<ITweetDayRepository> _tweetDayRepository;
+        //private readonly ITweetDayRepository _tweetDayRepository;
 
         public TweetsControllerTests()
         {
-            //_tweetDayRepository = new Mock<ITweetDayRepository>();
+            //_tweetDayRepository = new TweetDayRepository(new ApplicationDbCon);
             _cache = new Mock<IMemoryCache>();
             _mapper = new Mock<IMapper>();
             _systemClock = new Mock<ISystemClockService>();
             _logger = new Mock<ILogger<TweetsController>>();
-
         }
 
         [Fact]
-        public async Task GetTweetDay_ReturnsAViewResult_WithAListOfBrainstormSessions()
+        public async Task GetTweetDay_ReturnsOkObjectResult_WithATweetObject()
         {
             // Arrange
             var mockRepo = new Mock<ITweetDayRepository>();
-            mockRepo.Setup(repo => repo.GetTweetOfTheDayAsync(DateTime.Today))
-                .ReturnsAsync(GetTestSession());
+            //mockRepo.Setup(repo => repo.GetTweetOfTheDayAsync(DateTime.Today))
+            //     .ReturnsAsync(GetTestTweet()); //--> needs a real SystemClockService
+            mockRepo.Setup(repo => repo.GetTweetOfTheDayAsync(It.IsAny<DateTime>()))
+                .ReturnsAsync(GetTestTweet());
 
-            var controller = new TweetsController(mockRepo.Object, _cache.Object, _logger.Object,
+            var myCache = new MemoryCache(new MemoryCacheOptions());
+
+            //var entryMock = new Mock<ICacheEntry>();
+            //myCache.Setup(m => m.CreateEntry(It.IsAny<object>())
+            //               .Returns(entryMock.Object));
+
+            //var myCache = new MemoryCache(new MemoryCacheOptions());
+            //var memCache = new MemoryCache(MemoryCacheOptions.)
+
+            var controller = new TweetsController(mockRepo.Object, myCache, _logger.Object,
                                                      _systemClock.Object, _mapper.Object);
 
-            //// Act
-            //var result = await controller.Index();
+            // Act
+            var result = await controller.GetTweetDay();
 
-            //// Assert
-            //var viewResult = Assert.IsType<ViewResult>(result);
-            //var model = Assert.IsAssignableFrom<IEnumerable<StormSessionViewModel>>(
-            //    viewResult.ViewData.Model);
-            //Assert.Equal(2, model.Count());
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
         }
 
-        private TweetDay GetTestSession()
+        private TweetDay GetTestTweet()
         {
-            var session = new TweetDay()
+            var tweet = new TweetDay()
             {
-
+                Bird = new Bird(),
+                BirdId = 0,
+                CreationDate = new DateTime(2016, 7, 2),
+                DisplayDay = new DateTime(2016, 7, 2),
+                LastUpdateDate = new DateTime(2016, 7, 2),
+                TweetDayId = 0
             };
 
-            return session;
+            return tweet;
         }
 
         private IEnumerable<TweetDay> GetTestSessions()
