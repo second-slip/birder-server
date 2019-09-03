ffc2804804907177fe41894aaa7201c1fbfb0ee0
A S Cross <andrew-stuart-cross@outlook.com>
2019-09-03 09:59:59 +0100

    about component ViewEncapsulation.None

--- Birder/ClientApp/src/app/about/about.component.ts
+++ Birder/ClientApp/src/app/about/about.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewEncapsulation } from '@angular/compiler/src/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AboutComponent implements OnInit {
 
