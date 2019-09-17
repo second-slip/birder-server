864e97783cc93334f1dafa9fc54e29535becd4ce
A S Cross <andrew-stuart-cross@outlook.com>
2019-09-17 17:40:40 +0100

    Test added to BirdsController.GetBirdsAsync

--- Birder.Tests/Controller/BirdsControllerTests.cs
+++ Birder.Tests/Controller/BirdsControllerTests.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Birder.Controllers;
+using Birder.Data;
 using Birder.Data.Model;
 using Birder.Data.Repository;
 using Birder.Services;
@@ -18,14 +19,96 @@ namespace Birder.Tests.Controller
     public class BirdsControllerTests
     {
         private IMemoryCache _cache;
-        private readonly Mock<IMapper> _mapper;
+        private readonly IMapper _mapper;
         private readonly Mock<ILogger<BirdsController>> _logger;
 
         public BirdsControllerTests()
         {
             _cache = new MemoryCache(new MemoryCacheOptions()); 
-            _mapper = new Mock<IMapper>();
             _logger = new Mock<ILogger<BirdsController>>();
+            var mappingConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new BirderMappingProfile());
+            });
+            _mapper = mappingConfig.CreateMapper();
+        }
+
+        #region GetBirds (Collection)
+
+        [Fact]
+        public async Task GetBirds_ReturnsOkObjectResult_WithABirdsObject()
+        {
+            // Arrange
+            var mockRepo = new Mock<IBirdRepository>();
+            mockRepo.Setup(repo => repo.GetBirdSummaryListAsync())
+                 .ReturnsAsync(GetTestBirds()); //--> needs a real SystemClockService
+            //    mockRepo.Setup(repo => repo.GetTweetOfTheDayAsync(It.IsAny<DateTime>()))
+            //        .ReturnsAsync(GetTestTweetDay());
+
+            var controller = new BirdsController(_mapper, _cache, _logger.Object, mockRepo.Object);
+
+            // Act
+            var result = await controller.GetBirdsAsync(BirderStatus.Common);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+        }
+
+
+
+        #endregion
+
+        private IEnumerable<Bird> GetTestBirds()
+        {
+            var birds = new List<Bird>();
+            birds.Add(new Bird
+            {
+                BirdId = 1,
+                Class = "",
+                Order = "",
+                Family = "",
+                Genus = "",
+                Species = "",
+                EnglishName = "Test species 1",
+                InternationalName = "",
+                Category = "",
+                PopulationSize = "",
+                BtoStatusInBritain = "",
+                ThumbnailUrl = "",
+                SongUrl = "",
+                CreationDate = DateTime.Now.AddDays(-4),
+                LastUpdateDate = DateTime.Now.AddDays(-4),
+                ConservationStatusId = 0,
+                Observations = null,
+                BirdConservationStatus = null,
+                BirderStatus = BirderStatus.Common,
+                TweetDay = null
+            });
+            birds.Add(new Bird
+            {
+                BirdId = 2,
+                Class = "",
+                Order = "",
+                Family = "",
+                Genus = "",
+                Species = "",
+                EnglishName = "Test species 2",
+                InternationalName = "",
+                Category = "",
+                PopulationSize = "",
+                BtoStatusInBritain = "",
+                ThumbnailUrl = "",
+                SongUrl = "",
+                CreationDate = DateTime.Now.AddDays(-4),
+                LastUpdateDate = DateTime.Now.AddDays(-4),
+                ConservationStatusId = 0,
+                Observations = null,
+                BirdConservationStatus = null,
+                BirderStatus = BirderStatus.Common,
+                TweetDay = null
+            });
+
+            return birds;
         }
     }
 }
