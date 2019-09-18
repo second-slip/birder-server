39f54ea463eaf34637ee948fc4388f7f1acc82e9
A S Cross <andrew-stuart-cross@outlook.com>
2019-09-18 14:26:26 +0100

    AuthenticationControllerTests added

---
+++ Birder.Tests/Controller/AuthenticationControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper.Configuration;
+using Birder.Data.Model;
+using Birder.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Birder.Tests.Controller
+{
+    public class AuthenticationControllerTests
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ILogger _logger;
+        private readonly ISystemClockService _systemClock;
+        private readonly IConfiguration _config;
+
+
+        public AuthenticationControllerTests()
+        {
+
+        }
+    }
+}
