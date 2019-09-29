695073418986782e89664f71bbac1c4be3e78973
A S Cross <andrew-stuart-cross@outlook.com>
2019-09-29 15:51:50 +0100

    UpdateProfileAsync_ReturnsBadRequest_WithModelStateError()

--- Birder.Tests/Controller/ManageControllerTests.cs
+++ Birder.Tests/Controller/ManageControllerTests.cs
@@ -11,10 +11,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Birder.Tests.Controller
 {
@@ -119,6 +121,60 @@ namespace Birder.Tests.Controller
         }
 
         #endregion
+
+
+        #region UpdateProfileAsync unit tests
+
+        [Fact]
+        public async Task UpdateProfileAsync_ReturnsBadRequest_WithModelStateError()
+        {
+            // Arrange
+            var mockUserManager = SharedFunctions.InitialiseMockUserManager();
+            //mockUserManager.Setup(repo => repo.FindByNameAsync(It.IsAny<string>()))
+            //               .ReturnsAsync(GetValidTestUser());
+
+            var controller = new ManageController(_mapper, _emailSender.Object, _urlService.Object, _logger.Object, mockUserManager.Object);
+
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = SharedFunctions.GetTestClaimsPrincipal() }
+            };
+
+            //Add model error
+            controller.ModelState.AddModelError("Test", "This is a test model error");
+
+            var model = new ManageProfileViewModel() { UserName = "", IsEmailConfirmed = true };
+
+            // Act
+            var result = await controller.UpdateProfileAsync(model);
+
+            // Assert
+            var modelState = controller.ModelState;
+            Assert.Equal(1, modelState.ErrorCount);
+            Assert.True(modelState.ContainsKey("Test"));
+            Assert.True(modelState["Test"].Errors.Count == 1);
+            Assert.Equal("This is a test model error", modelState["Test"].Errors[0].ErrorMessage);
+
+            Assert.IsType<BadRequestObjectResult>(result);
+            var objectResult = result as ObjectResult;
+            Assert.NotNull(objectResult);
+            Assert.True(objectResult is BadRequestObjectResult);
+            Assert.Equal(StatusCodes.Status400BadRequest, objectResult.StatusCode);
+            //Assert.IsType<String>(objectResult.Value);
+
+            // Assert
+            var returnError = Assert.IsType<SerializableError>(objectResult.Value);
+            Assert.Single(returnError); //Assert.Equal(2, returnError.Count);
+            Assert.True(returnError.ContainsKey("Test"));
+
+            var values = returnError["Test"] as String[];
+            Assert.True(values[0] == "This is a test model error");
+        }
+
+
+        #endregion
+
+
 
 
         private ApplicationUser GetValidTestUser()
