ed22371ef36fce560bd1deb8af9fee45240b3450
A S Cross <andrew-stuart-cross@outlook.com>
2019-10-06 19:10:20 +0100

    Unit tests GetUserProfileAsync

--- Birder.Tests/Controller/UserControllerTests.cs
+++ Birder.Tests/Controller/UserControllerTests.cs
@@ -22,9 +22,11 @@ namespace Birder.Tests.Controller
     {
         private readonly IMapper _mapper;
         private readonly Mock<ILogger<UserController>> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
 
         public UserControllerTests()
         {
+            _userManager = SharedFunctions.InitialiseUserManager();
             var mappingConfig = new MapperConfiguration(cfg =>
             {
                 cfg.AddProfile(new BirderMappingProfile());
@@ -37,42 +39,42 @@ namespace Birder.Tests.Controller
         #region GetUser action tests
 
         [Fact]
-        public async Task GetUserAsync_ReturnsBadRequest_WhenRepositoryReturnsNull()
+        public async Task GetUserProfileAsync_ReturnsBadRequest_WhenRepositoryReturnsNull()
         {
             // Arrange
-            var mockUserManager = SharedFunctions.InitialiseUserManager();
+            //var userManager = SharedFunctions.InitialiseUserManager();
 
-            var t = await mockUserManager.GetUserWithNetworkAsync("Tenko");
+            //var t = await mockUserManager.GetUserWithNetworkAsync("Tenko");
             //.Returns(Task.FromResult<ApplicationUser>(null));
 
-            var controller = new UserController(_mapper, _logger.Object, mockUserManager);
+            var controller = new UserController(_mapper, _logger.Object, _userManager);
 
             controller.ControllerContext = new ControllerContext()
             {
                 HttpContext = new DefaultHttpContext() { User = GetTestClaimsPrincipal2("NonExistentUsername") }
             };
 
-            string username = "Tenko";
+            string requestedUsername = "This requested user does not exist";
 
             // Act
-            var result = await controller.GetUserProfileAsync(username);
+            var result = await controller.GetUserProfileAsync(requestedUsername);
 
             // Assert
             var objectResult = Assert.IsType<NotFoundObjectResult>(result);
             Assert.IsType<String>(objectResult.Value);
-            Assert.Equal("User not found", objectResult.Value);
+            Assert.Equal("Requested user not found", objectResult.Value);
         }
 
         [Fact] //Requesting own profile
-        public async Task GetUserAsync_ReturnsOkObjectResult_WithOwnUserProfileViewModelObject()
+        public async Task GetUserProfileAsync_ReturnsOkObjectResult_WithOwnUserProfileViewModelObject()
         {
             // Arrange
-            var mockUserManager = new Mock<UserManager<ApplicationUser>>();
-            mockUserManager.Setup(repo => repo.GetUserWithNetworkAsync(It.IsAny<string>()))
-                 .ReturnsAsync(GetOwnUserProfile());
+            //var mockUserManager = new Mock<UserManager<ApplicationUser>>();
+            //mockUserManager.Setup(repo => repo.GetUserWithNetworkAsync(It.IsAny<string>()))
+            //     .ReturnsAsync(GetOwnUserProfile());
 
 
-            var controller = new UserController(_mapper, _logger.Object, mockUserManager.Object);
+            var controller = new UserController(_mapper, _logger.Object, _userManager);
             controller.ControllerContext = new ControllerContext()
             {
                 HttpContext = new DefaultHttpContext() { User = SharedFunctions.GetTestClaimsPrincipal() }
@@ -96,15 +98,15 @@ namespace Birder.Tests.Controller
         }
 
         [Fact] //Requesting other member's profile
-        public async Task GetUserAsync_ReturnsOkObjectResult_WithOtherMembersUserProfileViewModelObject()
+        public async Task GetUserProfileAsync_ReturnsOkObjectResult_WithOtherMembersUserProfileViewModelObject()
         {
             // Arrange
-            var mockUserManager = new Mock<UserManager<ApplicationUser>>();
-            mockUserManager.Setup(repo => repo.GetUserWithNetworkAsync(It.IsAny<string>()))
-                 .ReturnsAsync(GetOtherMemberUserProfile());
+            //var mockUserManager = new Mock<UserManager<ApplicationUser>>();
+            //mockUserManager.Setup(repo => repo.GetUserWithNetworkAsync(It.IsAny<string>()))
+            //     .ReturnsAsync(GetOtherMemberUserProfile());
 
 
-            var controller = new UserController(_mapper, _logger.Object, mockUserManager.Object);
+            var controller = new UserController(_mapper, _logger.Object, _userManager);
             controller.ControllerContext = new ControllerContext()
             {
                 HttpContext = new DefaultHttpContext() { User = SharedFunctions.GetTestClaimsPrincipal() }
@@ -128,14 +130,14 @@ namespace Birder.Tests.Controller
         }
 
         [Fact]
-        public async Task GetUserAsync_ReturnsBadRequestResult_WhenExceptionIsRaised()
+        public async Task GetUserProfileAsync_ReturnsBadRequestResult_WhenExceptionIsRaised()
         {
             // Arrange
-            var mockUserManager = new Mock<UserManager<ApplicationUser>>();
-            mockUserManager.Setup(repo => repo.GetUserWithNetworkAsync(It.IsAny<string>()))
-                 .ThrowsAsync(new InvalidOperationException());
+            //var mockUserManager = new Mock<UserManager<ApplicationUser>>();
+            //mockUserManager.Setup(repo => repo.GetUserWithNetworkAsync(It.IsAny<string>()))
+            //     .ThrowsAsync(new InvalidOperationException());
 
-            var controller = new UserController(_mapper, _logger.Object, mockUserManager.Object);
+            var controller = new UserController(_mapper, _logger.Object, _userManager);
             controller.ControllerContext = new ControllerContext()
             {
                 HttpContext = new DefaultHttpContext() { User = SharedFunctions.GetTestClaimsPrincipal() }
