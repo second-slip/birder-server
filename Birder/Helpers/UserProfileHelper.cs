410932036fbad0406a21fdfb9b57b7da9dbd6ad8
A S Cross <andrew-stuart-cross@outlook.com>
2019-10-06 16:39:54 +0100

    Refactor UserProfileHelper methods

--- Birder/Helpers/UserProfileHelper.cs
+++ Birder/Helpers/UserProfileHelper.cs
@@ -10,15 +10,15 @@ namespace Birder.Helpers
         /// Updates the requested user's 'Following' collection (IsFollowing and IsOwnProfile properties)
         /// </summary>
         /// <param name="viewModel"></param>
-        /// <param name="loggedinUser"></param>
+        /// <param name="requestingUser"></param>
         /// <param name="loggedinUsername"></param>
         /// <returns></returns>
-        public static UserProfileViewModel UpdateFollowingCollection(UserProfileViewModel viewModel, ApplicationUser loggedinUser) //, string loggedinUsername)
+        public static UserProfileViewModel UpdateFollowingCollection(UserProfileViewModel viewModel, ApplicationUser requestingUser) //, string loggedinUsername)
         {
             for (int i = 0; i < viewModel.Following.Count(); i++)
             {
-                viewModel.Following.ElementAt(i).IsFollowing = loggedinUser.Following.Any(cus => cus.ApplicationUser.UserName == viewModel.Following.ElementAt(i).UserName);
-                viewModel.Following.ElementAt(i).IsOwnProfile = viewModel.Following.ElementAt(i).UserName == loggedinUser.UserName;
+                viewModel.Following.ElementAt(i).IsFollowing = requestingUser.Following.Any(cus => cus.ApplicationUser.UserName == viewModel.Following.ElementAt(i).UserName);
+                viewModel.Following.ElementAt(i).IsOwnProfile = viewModel.Following.ElementAt(i).UserName == requestingUser.UserName;
             }
 
             return viewModel;
@@ -28,15 +28,15 @@ namespace Birder.Helpers
         /// Updates the requested user's 'Followers' collection (IsFollowing and IsOwnProfile properties)
         /// </summary>
         /// <param name="viewModel"></param>
-        /// <param name="loggedinUser"></param>
+        /// <param name="requestingUser"></param>
         /// <param name="loggedinUsername"></param>
         /// <returns></returns>
-        public static UserProfileViewModel UpdateFollowersCollection(UserProfileViewModel viewModel, ApplicationUser loggedinUser) //, string loggedinUsername)
+        public static UserProfileViewModel UpdateFollowersCollection(UserProfileViewModel viewModel, ApplicationUser requestingUser) //, string loggedinUsername)
         {
             for (int i = 0; i < viewModel.Followers.Count(); i++)
             {
-                viewModel.Followers.ElementAt(i).IsFollowing = loggedinUser.Following.Any(cus => cus.ApplicationUser.UserName == viewModel.Followers.ElementAt(i).UserName);
-                viewModel.Followers.ElementAt(i).IsOwnProfile = viewModel.Followers.ElementAt(i).UserName == loggedinUser.UserName;
+                viewModel.Followers.ElementAt(i).IsFollowing = requestingUser.Following.Any(cus => cus.ApplicationUser.UserName == viewModel.Followers.ElementAt(i).UserName);
+                viewModel.Followers.ElementAt(i).IsOwnProfile = viewModel.Followers.ElementAt(i).UserName == requestingUser.UserName;
             }
 
             return viewModel;
