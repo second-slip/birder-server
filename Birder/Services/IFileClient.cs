34beed7b1c9d5eacb43d41e24e9d1ce796185f35
A S Cross <andrew-stuart-cross@outlook.com>
2019-10-09 17:58:21 +0100

    IFileClient added

---
+++ Birder/Services/IFileClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Birder.Services
+{
+    public interface IFileClient
+    {
+        Task DeleteFile(string storeName, string filePath);
+        Task<bool> FileExists(string storeName, string filePath);
+        Task<Stream> GetFile(string storeName, string filePath);
+        Task<string> GetFileUrl(string storeName, string filePath);
+        Task SaveFile(string storeName, string filePath, Stream fileStream);
+    }
+}
