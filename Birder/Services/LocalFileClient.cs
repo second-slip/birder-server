c69aa56cdc35d173830dea8f6ad7b7e3d1251416
A S Cross <andrew-stuart-cross@outlook.com>
2019-10-14 14:28:34 +0100

    LocalFileClient implemation added

---
+++ Birder/Services/LocalFileClient.cs
@@ -0,0 +1,67 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Birder.Services
+{
+    public class LocalFileClient : IFileClient
+    {
+        private string _fileRoot;
+
+        public LocalFileClient(string fileRoot)
+        {
+            _fileRoot = fileRoot;
+        }
+
+        public Task DeleteFile(string storeName, string filePath)
+        {
+            var path = Path.Combine(_fileRoot, storeName, filePath);
+
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+            return Task.CompletedTask;
+        }
+
+        public Task<bool> FileExists(string storeName, string filePath)
+        {
+            var path = Path.Combine(_fileRoot, storeName, filePath);
+
+            return Task.FromResult(File.Exists(path));
+        }
+
+        public Task<Stream> GetFile(string storeName, string filePath)
+        {
+            var path = Path.Combine(_fileRoot, storeName, filePath);
+            Stream stream = null;
+
+            if (File.Exists(path))
+            {
+                stream = File.OpenRead(path);
+            }
+
+            return Task.FromResult(stream);
+        }
+
+        public Task<string> GetFileUrl(string storeName, string filePath)
+        {
+            return Task.FromResult((string)null);
+        }
+
+        public async Task SaveFile(string storeName, string filePath, Stream fileStream)
+        {
+            var path = Path.Combine(_fileRoot, storeName, filePath);
+
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+
+            using (var file = new FileStream(path, FileMode.CreateNew))
+            {
+                await fileStream.CopyToAsync(file).ConfigureAwait(false);
+            }
+        }
+    }
+}
+    
