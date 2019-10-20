3c81e8e4c1cd6721f2fa0e705fd8a7fb169fb1ea
A S Cross <andrew-stuart-cross@outlook.com>
2019-10-20 23:26:43 +0100

    observation feed uses infinite scroll

--- Birder/ClientApp/src/app/_observationsFeed/observation-feed/observation-feed.component.ts
+++ Birder/ClientApp/src/app/_observationsFeed/observation-feed/observation-feed.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { ObservationViewModel } from '../../_models/ObservationViewModel';
-import { Router } from '@angular/router';
-import { ErrorReportViewModel } from '../../_models/ErrorReportViewModel';
-import { ObservationsFeedService } from '../observations-feed.service';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { tap, map, filter, debounceTime, distinct, flatMap } from 'rxjs/operators';
+import { BehaviorSubject, fromEvent, merge, Observable } from 'rxjs';
+import * as _ from 'lodash';
 import { ObservationFeedDto } from '@app/_models/ObservationFeedDto';
+import { ObservationsFeedService } from '@app/_observationsFeed/observations-feed.service';
+import { ErrorReportViewModel } from '@app/_models/ErrorReportViewModel';
+import { ObservationViewModel } from '@app/_models/ObservationViewModel';
 
 @Component({
   selector: 'app-observation-feed',
@@ -11,34 +13,107 @@ import { ObservationFeedDto } from '@app/_models/ObservationFeedDto';
   styleUrls: ['./observation-feed.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class ObservationFeedComponent implements OnInit {
-  observations: ObservationViewModel[];
+export class ObservationFeedComponent {
 
-  constructor(private observationsFeedService: ObservationsFeedService
-    , private router: Router) { }
+  private allLoaded = false;
+  private cache = [];
+  private pageByManual$ = new BehaviorSubject(1);
+  private itemHeight = 145;
+  private numberOfItems = 10;
+  private pageByScroll$ = fromEvent(window, 'scroll')
+    .pipe(
+      map(() => window.scrollY),
+      filter(current => current >= document.body.clientHeight - window.innerHeight),
+      debounceTime(200),
+      distinct(),
+      map(y => Math.ceil((y + window.innerHeight) / (this.itemHeight * this.numberOfItems)))
+    );
 
-  ngOnInit() {
-    this.getObservations();
+  private pageByResize$ = fromEvent(window, 'resize')
+    .pipe(
+      debounceTime(200),
+      map(_ => Math.ceil(
+        (window.innerHeight + document.body.scrollTop) / (this.itemHeight * this.numberOfItems)
+      ))
+    );
+
+  private pageToLoad$ = merge(this.pageByManual$, this.pageByScroll$, this.pageByResize$)
+    .pipe(
+      distinct(),
+      filter(page => this.cache[page - 1] === undefined)
+    );
+
+  loading = false;
+
+  itemResults$: Observable<ObservationViewModel[]> = this.pageToLoad$ // itemResults$: ObservationFeedDto
+    .pipe(
+      tap(_ => this.loading = true),
+
+      flatMap((page: number) => {
+        // check max page reached?
+        // if (!this.x) {
+        // doesn't seem necessary as 'white space' check is adequate
+
+        return this.observationsFeedService.getObservationsFeed1(page)
+          .pipe(
+            tap((resp: ObservationFeedDto) => {
+              // this.n = resp.totalItems;
+
+              if (page === Math.ceil(<number>resp.totalItems / <number>this.numberOfItems)) { this.allLoaded = true; }
+            },
+              (error: ErrorReportViewModel) => {
+                // this.router.navigate(['/page-not-found']);
+              }),
+            map((resp: any) => resp.items), // resp.results),
+            tap(resp => {
+              this.cache[page - 1] = resp;
+              if ((this.itemHeight * this.numberOfItems * page) < window.innerHeight) {
+                this.pageByManual$.next(page + 1);
+              }
+            }),
+          );
+        // }
+      }),
+      map(() => _.flatMap(this.cache))
+    );
+
+  constructor(private observationsFeedService: ObservationsFeedService) {
   }
-
-  getObservations(): void {
-    this.observationsFeedService.getObservationsFeed()
-      .subscribe(
-        (response: ObservationViewModel[]) => {
-          this.observations = response;
-        },
-        (error: ErrorReportViewModel) => {
-          this.router.navigate(['/page-not-found']);
-        });
-  }
-  // getObservations(): void {
-  //   this.observationsFeedService.getObservationsFeed()
-  //     .subscribe(
-  //       (response: ObservationViewModel[]) => {
-  //         this.observations = response;
-  //       },
-  //       (error: ErrorReportViewModel) => {
-  //         this.router.navigate(['/page-not-found']);
-  //       });
-  // }
 }
+
+
+
+
+// import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+// import { ObservationViewModel } from '../../_models/ObservationViewModel';
+// import { Router } from '@angular/router';
+// import { ErrorReportViewModel } from '../../_models/ErrorReportViewModel';
+// import { ObservationsFeedService } from '../observations-feed.service';
+
+// @Component({
+//   selector: 'app-observation-feed',
+//   templateUrl: './observation-feed.component.html',
+//   styleUrls: ['./observation-feed.component.scss'],
+//   encapsulation: ViewEncapsulation.None
+// })
+// export class ObservationFeedComponent implements OnInit {
+//   observations: ObservationViewModel[];
+
+//   constructor(private observationsFeedService: ObservationsFeedService
+//     , private router: Router) { }
+
+//   ngOnInit() {
+//     this.getObservations();
+//   }
+
+//   getObservations(): void {
+//     this.observationsFeedService.getObservationsFeed()
+//       .subscribe(
+//         (response: ObservationViewModel[]) => {
+//           this.observations = response;
+//         },
+//         (error: ErrorReportViewModel) => {
+//           this.router.navigate(['/page-not-found']);
+//         });
+//   }
+// }
