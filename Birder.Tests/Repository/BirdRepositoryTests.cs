39b7840acddcaeaffdaa464618804679cc766c4d
A S Cross <andrew-stuart-cross@outlook.com>
2019-11-11 00:18:59 +0000

    Bird repository tests

---
+++ Birder.Tests/Repository/BirdRepositoryTests.cs
@@ -0,0 +1,39 @@
+﻿using Birder.Data;
+using Birder.Data.Repository;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Birder.Tests.Repository
+{
+    public class BirdRepositoryTests
+    {
+        [Fact]
+        public async System.Threading.Tasks.Task GetAuthor_EmptyGuid_ThrowsArgumentException()
+        {
+            // Arrange
+            var connectionStringBuilder =
+                new SqliteConnectionStringBuilder { DataSource = ":memory:" };
+            var connection = new SqliteConnection(connectionStringBuilder.ToString());
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlite(connection)
+                .Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.Database.OpenConnection();
+                context.Database.EnsureCreated();
+
+                var authorRepository = new BirdRepository(context);
+
+                // Assert
+                await Assert.ThrowsAsync<ArgumentException>(
+                    // Act      
+                    () => authorRepository.GetBirdAsync(0));
+            }
+        }
+    }
+}
--- Birder/Data/Repository/BirdRepository.cs
+++ Birder/Data/Repository/BirdRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Birder.Data.Model;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,11 +17,15 @@ namespace Birder.Data.Repository
 
         public Task<Bird> GetBirdAsync(int id)
         {
+            if (id == 0)
+            {
+                throw new ArgumentException(nameof(id));
+            }
+
             return (from b in _dbContext.Birds
                     .Include(cs => cs.BirdConservationStatus)
                     where (b.BirdId == id)
                     select b).FirstOrDefaultAsync();
-            //return await _dbContext.Birds.SingleOrDefaultAsync(m => m.BirdId == id);
         }
 
         public async Task<IEnumerable<Bird>> GetBirdSummaryListAsync()
