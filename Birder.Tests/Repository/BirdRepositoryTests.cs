f2b99db8f9d51f62be21643addd96b63d5e80a30
A S Cross <andrew-stuart-cross@outlook.com>
2019-11-11 13:02:04 +0000

    Tidy finished BirdRepoTests

--- Birder.Tests/Repository/BirdRepositoryTests.cs
+++ Birder.Tests/Repository/BirdRepositoryTests.cs
@@ -4,9 +4,7 @@ using Birder.Data.Repository;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
