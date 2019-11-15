f3239443f187e8e150a3701ea7c56b43e26848a3
A S Cross <andrew-stuart-cross@outlook.com>
2019-11-15 23:29:23 +0000

    Obsolete test code removed

--- Birder.Tests/Services/LocalFileClientTests.cs
+++ Birder.Tests/Services/LocalFileClientTests.cs
@@ -16,32 +16,5 @@ namespace Birder.Tests.Services
         {
             _fileClient = new LocalFileClient(@"C:\Users\Andre\OneDrive");
         }
-
-        [Fact]
-        public async Task x_y_z()
-        {
-
-
-            string a = "https://birderstorage.blob.core.windows.net/profile/birder.png";
-            a.Replace("birder.png", "test");
-
-            var t = 4;
-
-            
-
-            //var a = await _fileClient.GetFile("Desktop", "BirderDictionaryDefinitionExample.png");
-
-            //await _fileClient.SaveFile("Birder", "Test.png", a);
-
-            //var fileName = Sanitize("/" + UserId + "/" + file.FileName);
-
-            //using (var fileStream = file.OpenReadStream())
-            //{
-            //    await _fileClient.SaveFile("Contracts", fileName, fileStream);
-            //}
-
-            Assert.NotNull(a);
-
-        }
     }
 }
