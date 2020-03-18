b1de6078f9c18b3ab23f8713a189b734ef95ff1d
A S Cross <andrew-stuart-cross@outlook.com>
2020-03-18 16:03:53 +0000

    more work on tests

--- Birder/ClientApp/src/app/_info/info-tweet-day/info-tweet-day.component.spec.ts
+++ Birder/ClientApp/src/app/_info/info-tweet-day/info-tweet-day.component.spec.ts
@@ -3,12 +3,14 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { InfoTweetDayComponent } from './info-tweet-day.component';
 import { TweetsService } from '@app/_services/tweets.service';
 import { of } from 'rxjs';
+import { TweetDay } from '@app/_models/TweetDay';
 
 describe('InfoTweetDayComponent', () => {
   let component: InfoTweetDayComponent;
   let fixture: ComponentFixture<InfoTweetDayComponent>;
 
   let mockTweetsService;
+  let heroes: TweetDay;
 
   beforeEach(async(() => {
     mockTweetsService  = jasmine.createSpyObj(['getTweetDay']);
@@ -25,7 +27,14 @@ describe('InfoTweetDayComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(InfoTweetDayComponent);
     component = fixture.componentInstance;
-    mockTweetsService.getTweetDay.and.returnValue(of(null));
+
+    let myObj = {tweetDayId: 11, displayDay: new Date(Date.now()), creationDate: new Date(Date.now()),
+    lastUpdateDate: new Date(Date.now()),
+    bird: {birdId: 1, species: '', englishName: '', populationSize: '', btoStatusInBritain: '',
+     thumbnailUrl: '', songUrl: '', conservationStatus: '', conservationListColourCode: '', birderStatus: '' } };
+    heroes = myObj;
+
+    mockTweetsService.getTweetDay.and.returnValue(of(heroes));
     fixture.detectChanges();
   });
 
