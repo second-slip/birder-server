b2811b673ec13efa5262cb16ea49f4cf34a6e651
A S Cross <andrew-stuart-cross@outlook.com>
2020-03-18 23:43:18 +0000

    AuthenticationService test dependencies

--- Birder/ClientApp/src/app/_services/auth-guard.service.ts
+++ Birder/ClientApp/src/app/_services/auth-guard.service.ts
@@ -5,7 +5,8 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable()
 export class AuthGuard implements CanActivate, CanActivateChild {
-  constructor(private jwtHelper: JwtHelperService
+  
+    constructor(private jwtHelper: JwtHelperService
             , private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
--- Birder/ClientApp/src/app/_services/authentication.service.spec.ts
+++ Birder/ClientApp/src/app/_services/authentication.service.spec.ts
@@ -1,10 +1,28 @@
 import { TestBed } from '@angular/core/testing';
 
-
 import { AuthenticationService } from './authentication.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpErrorHandlerService } from './http-error-handler.service';
+import { JwtHelperService, JwtModule } from '@auth0/angular-jwt';
+import { environment } from 'environments/environment';
+import { tokenGetter } from '@app/app.module';
 
 describe('AuthenticationService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ HttpClientTestingModule, JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        // Exclude this URL from JWT (doesn't add the authentication header)
+        blacklistedRoutes: [
+          '/api/login',
+        ]
+      }
+    }), ],
+    providers: [
+      AuthenticationService, HttpErrorHandlerService, JwtHelperService
+      // { provide: HttpErrorHandlerService, useValue: mockHttpErrorHandlerService }
+    ]
+  }));
 
   it('should be created', () => {
     const service: AuthenticationService = TestBed.inject(AuthenticationService);
