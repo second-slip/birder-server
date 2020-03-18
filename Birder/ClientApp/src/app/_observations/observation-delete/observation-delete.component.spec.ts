ffcb8101472217fb3573a3c2c3c3d1213d5de43b
A S Cross <andrew-stuart-cross@outlook.com>
2020-03-18 15:13:29 +0000

    obs delete test dependencies

--- Birder/ClientApp/src/app/_observations/observation-delete/observation-delete.component.spec.ts
+++ Birder/ClientApp/src/app/_observations/observation-delete/observation-delete.component.spec.ts
@@ -3,12 +3,18 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ObservationDeleteComponent } from './observation-delete.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ObservationService } from '@app/_services/observation.service';
+import { ToastrService } from 'ngx-toastr';
+import { TokenService } from '@app/_services/token.service';
+import { of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 describe('ObservationDeleteComponent', () => {
   let component: ObservationDeleteComponent;
   let fixture: ComponentFixture<ObservationDeleteComponent>;
 
   let mockObservationService;
+  let mockToastr;
+  let mockTokenService;
 
   beforeEach(async(() => {
     mockObservationService = jasmine.createSpyObj(['getObservation', 'deleteObservation']);
@@ -21,7 +27,11 @@ describe('ObservationDeleteComponent', () => {
       ],
       declarations: [ ObservationDeleteComponent ],
       providers: [
-        { provide: ObservationService, useValue: mockObservationService }
+        { provide: ObservationService, useValue: mockObservationService },
+        { provide: TokenService, useValue: mockTokenService },
+        { provide: ToastrService, useValue: mockToastr },
+        { provide: Location, useValue: window.location },
+        { provide: ActivatedRoute, useValue: { params: of([{id: 1}]) } }
       ]
     })
     .compileComponents();
@@ -30,6 +40,8 @@ describe('ObservationDeleteComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ObservationDeleteComponent);
     component = fixture.componentInstance;
+    mockObservationService.getObservation.and.returnValue(of(null));
+    mockObservationService.deleteObservation.and.returnValue(of(null));
     fixture.detectChanges();
   });
 
