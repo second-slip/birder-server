cdd28ab6681ad269ea593f27f7d23fa02c35e96e
A S Cross <andrew-stuart-cross@outlook.com>
2020-03-19 13:10:20 +0000

    UserNetwork test dependencies added

--- Birder/ClientApp/src/app/_network/user-network/user-network.component.spec.ts
+++ Birder/ClientApp/src/app/_network/user-network/user-network.component.spec.ts
@@ -1,14 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserNetworkComponent } from './user-network.component';
+import { NetworkService } from '@app/_services/network.service';
+import { of } from 'rxjs';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 
 describe('UserNetworkComponent', () => {
   let component: UserNetworkComponent;
   let fixture: ComponentFixture<UserNetworkComponent>;
 
+  let mockNetworkService;
+  let mockToastr;
+
   beforeEach(async(() => {
+    mockNetworkService = jasmine.createSpyObj(['getSearchNetwork', 'getNetworkSuggestions', 'postFollowUser', 'postUnfollowUser']);
+    mockToastr = jasmine.createSpyObj(['info', 'error']);
+
     TestBed.configureTestingModule({
-      declarations: [ UserNetworkComponent ]
+      imports: [ToastrModule.forRoot()],
+      declarations: [ UserNetworkComponent ],
+      providers: [
+        { provide: NetworkService, useValue: mockNetworkService },
+        { provide: ToastrService, useValue: mockToastr }
+       ]
     })
     .compileComponents();
   }));
@@ -16,6 +30,11 @@ describe('UserNetworkComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UserNetworkComponent);
     component = fixture.componentInstance;
+    mockNetworkService.getSearchNetwork.and.returnValue(of(null));
+    mockNetworkService.getNetworkSuggestions.and.returnValue(of(null));
+    mockNetworkService.postFollowUser.and.returnValue(of(null));
+    mockNetworkService.postUnfollowUser.and.returnValue(of(null));
+    
     fixture.detectChanges();
   });
 
