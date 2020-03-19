3e4c4ed4fbddc9ccc44a2124402278a640bef2a5
A S Cross <andrew-stuart-cross@outlook.com>
2020-03-19 11:52:46 +0000

    TokenService test dependencies added

--- Birder/ClientApp/src/app/_services/token.service.spec.ts
+++ Birder/ClientApp/src/app/_services/token.service.spec.ts
@@ -1,9 +1,25 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TokenService } from './token.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { JwtModule } from '@auth0/angular-jwt';
+import { tokenGetter } from '@app/app.module';
 
 describe('TokenService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ HttpClientTestingModule, JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        // Exclude this URL from JWT (doesn't add the authentication header)
+        blacklistedRoutes: [
+          '/api/login',
+        ]
+      }
+    }), ],
+    providers: [
+      TokenService
+    ]
+  }));
 
   it('should be created', () => {
     const service: TokenService = TestBed.inject(TokenService);
