8a5841047a56e2075a667688468783f7f42bba25
A S Cross <andrew-stuart-cross@outlook.com>
2020-03-19 17:37:56 +0000

    register/login test dependencies added

--- Birder/ClientApp/src/app/_account/register/register.component.spec.ts
+++ Birder/ClientApp/src/app/_account/register/register.component.spec.ts
@@ -2,15 +2,28 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
 import { FormBuilder } from '@angular/forms';
+import { AccountService } from '@app/_services/account.service';
+import { of } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
 
+  let mockAccountService;
+
   beforeEach(async(() => {
+    mockAccountService = jasmine.createSpyObj(['checkValidUsername', 'register']);
+
     TestBed.configureTestingModule({
+      imports: [ RouterTestingModule.withRoutes([
+          // { path: 'login', component: DummyLoginLayoutComponent },
+        ])
+      ],
       declarations: [ RegisterComponent ],
-      providers: [ FormBuilder ]
+      providers: [ FormBuilder,
+        { provide: AccountService, useValue: mockAccountService },
+      ]
     })
     .compileComponents();
   }));
@@ -18,6 +31,8 @@ describe('RegisterComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
+    mockAccountService.register.and.returnValue(of(null));
+    mockAccountService.checkValidUsername.and.returnValue(of(true));
     fixture.detectChanges();
   });
 
