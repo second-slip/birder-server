14aec1646c76308f295c68a66ff7ac0ec549c5bb
A S Cross <andrew-stuart-cross@outlook.com>
2020-03-19 13:10:07 +0000

    Avatar test dependencies added

--- Birder/ClientApp/src/app/_accountManager/account-manager-avatar/account-manager-avatar.component.spec.ts
+++ Birder/ClientApp/src/app/_accountManager/account-manager-avatar/account-manager-avatar.component.spec.ts
@@ -1,14 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AccountManagerAvatarComponent } from './account-manager-avatar.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { AccountManagerService } from '@app/_services/account-manager.service';
+import { of } from 'rxjs';
 
 describe('AccountManagerAvatarComponent', () => {
   let component: AccountManagerAvatarComponent;
   let fixture: ComponentFixture<AccountManagerAvatarComponent>;
 
+  let mockAccountManagerService;
+  let mockToastr;
+
   beforeEach(async(() => {
+    mockAccountManagerService = jasmine.createSpyObj(['postAvatar']);
+    mockToastr = jasmine.createSpyObj(['success']);
+
     TestBed.configureTestingModule({
-      declarations: [ AccountManagerAvatarComponent ]
+      imports: [ ToastrModule.forRoot(),
+        RouterTestingModule.withRoutes([
+          // { path: 'login', component: DummyLoginLayoutComponent },
+        ])
+      ],
+      declarations: [ AccountManagerAvatarComponent ],
+      providers: [
+        { provide: AccountManagerService, useValue: mockAccountManagerService },
+        { provide: ToastrService, useValue: mockToastr }
+      ]
     })
     .compileComponents();
   }));
@@ -16,6 +35,7 @@ describe('AccountManagerAvatarComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AccountManagerAvatarComponent);
     component = fixture.componentInstance;
+    mockAccountManagerService.postAvatar.and.returnValue(of(null));
     fixture.detectChanges();
   });
 
