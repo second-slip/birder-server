7b225c8b267b55cb39865d7837749613fe2a9625
A S Cross <andrew-stuart-cross@outlook.com>
2020-03-23 23:30:47 +0000

    Tidying up post refactor

--- Birder/ClientApp/src/app/observations-list/observations-list.component.spec.ts
+++ Birder/ClientApp/src/app/observations-list/observations-list.component.spec.ts
@@ -23,3 +23,25 @@ describe('ObservationsListComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+
+// this.observations = [];
+// // this.isActived = true;
+// let myObj: ObservationViewModel = { observationId: 1, locationLatitude: 1, locationLongitude: 1,
+//   quantity: 1,
+//   noteGeneral: 'string',
+//   noteHabitat: 'string',
+//   noteWeather: 'string',
+//   noteAppearance: 'string',
+//   noteBehaviour: 'string',
+//   noteVocalisation: 'string',
+//   hasPhotos: true,
+//   // SelectedPrivacyLevel: PrivacyLevel;
+//   observationDateTime: 'string',
+//   creationDate: 'string',
+//   lastUpdateDate: 'string',
+//   birdId: 1,
+//   bird: null,
+//   user: null  };
+//   console.log(myObj);
+// this.observations.push(myObj);
--- Birder/ClientApp/src/app/observations-list/observations-list.component.ts
+++ Birder/ClientApp/src/app/observations-list/observations-list.component.ts
@@ -44,26 +44,3 @@ export class ObservationsListComponent implements OnInit {
         });
   }
 }
-
-
-
-// this.observations = [];
-// // this.isActived = true;
-// let myObj: ObservationViewModel = { observationId: 1, locationLatitude: 1, locationLongitude: 1,
-//   quantity: 1,
-//   noteGeneral: 'string',
-//   noteHabitat: 'string',
-//   noteWeather: 'string',
-//   noteAppearance: 'string',
-//   noteBehaviour: 'string',
-//   noteVocalisation: 'string',
-//   hasPhotos: true,
-//   // SelectedPrivacyLevel: PrivacyLevel;
-//   observationDateTime: 'string',
-//   creationDate: 'string',
-//   lastUpdateDate: 'string',
-//   birdId: 1,
-//   bird: null,
-//   user: null  };
-//   console.log(myObj);
-// this.observations.push(myObj);
