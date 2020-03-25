9f9ca154aae52eb8cb210e6cb6bfc3c6d28b3776
A S Cross <andrew-stuart-cross@outlook.com>
2020-03-25 11:47:15 +0000

    Implementation added

--- Birder/ClientApp/src/app/_sharedComponents/user-observations-list/user-observations-list.component.ts
+++ Birder/ClientApp/src/app/_sharedComponents/user-observations-list/user-observations-list.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { ObservationViewModel } from '@app/_models/ObservationViewModel';
+import { ErrorReportViewModel } from '@app/_models/ErrorReportViewModel';
+import { ObservationService } from '@app/_services/observation.service';
+import { ObservationDto } from '@app/_models/ObservationFeedDto';
 
 @Component({
   selector: 'app-user-observations-list',
   templateUrl: './user-observations-list.component.html',
-  styleUrls: ['./user-observations-list.component.scss']
+  styleUrls: ['./user-observations-list.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class UserObservationsListComponent implements OnInit {
+  observations: ObservationViewModel[];
+  @Input() birdId: number;
+  totalItems: number;
+  page = 1;
+  pageSize = 10;
 
-  constructor() { }
+  constructor(private observationsService: ObservationService) { }
 
   ngOnInit(): void {
+    if (!this.observations) {
+      this.getObservations(this.birdId, this.page, this.pageSize);
+    }
   }
 
+  changePage(): void { // event) { // page: number) {
+    this.getObservations(this.birdId, this.page, this.pageSize);
+  }
+
+  getObservations(birdId: number, page: number, pageSize: number): void {
+    this.observationsService.getObservationsByBirdSpecies(birdId, page, pageSize)
+      .subscribe(
+        (data: ObservationDto) => {
+          this.totalItems = data.totalItems;
+          this.observations = data.items;
+        },
+        (error: ErrorReportViewModel) => {
+          console.log('bad request');
+          // this.router.navigate(['/page-not-found']);  // TODO: this is right for typing bad param, but what about server error?
+        });
+        // () => {
+        // });
+  }
 }
