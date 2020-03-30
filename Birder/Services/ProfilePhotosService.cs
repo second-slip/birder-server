7b5c1135a902f2e99a16d5527068c1687d3708b2
A S Cross <andrew-stuart-cross@outlook.com>
2020-03-30 01:17:09 +0100

    ProfilePhotosService added

---
+++ Birder/Services/ProfilePhotosService.cs
@@ -0,0 +1,22 @@
+﻿using Birder.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Birder.Services
+{
+
+    public interface IProfilePhotosService
+    {
+        IEnumerable<Observation> GetProfilePhoto(IEnumerable<Observation> observations);
+    }
+
+    public class ProfilePhotosService : IProfilePhotosService
+    {
+        public IEnumerable<Observation> GetProfilePhoto(IEnumerable<Observation> observations)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
