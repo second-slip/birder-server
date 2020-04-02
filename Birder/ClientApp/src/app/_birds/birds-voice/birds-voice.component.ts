6aea033015f7065e81038066dc81594f16119018
A S Cross <andrew-stuart-cross@outlook.com>
2020-04-02 16:43:20 +0100

    Birds song function added to BirdDetail

--- Birder/ClientApp/src/app/_birds/birds-voice/birds-voice.component.ts
+++ Birder/ClientApp/src/app/_birds/birds-voice/birds-voice.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { XenoCantoService } from '@app/xeno-canto.service';
+import { IXenoCantoResponse } from '@app/_models/IXenoCantoResponse';
 
 @Component({
   selector: 'app-birds-voice',
   templateUrl: './birds-voice.component.html',
-  styleUrls: ['./birds-voice.component.scss']
+  styleUrls: ['./birds-voice.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class BirdsVoiceComponent implements OnInit {
   @Input() species: string;
+  recordings: IXenoCantoResponse;
+  page: number;
+  pageSize = 3;
 
-  constructor() { }
+  constructor(private xeno: XenoCantoService) { }
 
   ngOnInit(): void {
+    if (!this.recordings) {
+    this.loadRecordings();
+    }
+  }
+
+  loadRecordings(): void {
+    this.xeno.getRecordings(this.species)
+      .subscribe((results: IXenoCantoResponse) => {
+        results.recordings.length = 10;
+        this.recordings = results;
+        this.page = 1;
+      });
   }
 
 }
