011431931550a05fada87e4e5e71b4132f3f256a
A S Cross <andrew-stuart-cross@outlook.com>
2020-04-03 20:04:06 +0100

    Tweet service unit tests setup

--- Birder/ClientApp/src/app/_services/tweets.service.spec.ts
+++ Birder/ClientApp/src/app/_services/tweets.service.spec.ts
@@ -1,24 +1,74 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { TweetsService } from './tweets.service';
 import { HttpErrorHandlerService } from './http-error-handler.service';
+import { HttpClient } from '@angular/common/http';
+import { TweetDay } from '@app/_models/TweetDay';
+import { BirdSummaryViewModel } from '@app/_models/BirdSummaryViewModel';
 
 describe('TweetsService', () => {
-  // let mockHttpErrorHandlerService;
+  // let httpClient: HttpClient;
+  // let httpTestingController: HttpTestingController;
+  // let tweetsService: TweetsService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let tweetsService: TweetsService;
 
-  beforeEach(() => TestBed.configureTestingModule({
-    // mockHttpErrorHandlerService = jasmine.createSpyObj(['handleHttpError']);
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      // Import the HttpClient mocking services
+      imports: [HttpClientTestingModule],
+      // Provide the service-under-test and its dependencies
+      providers: [
+        TweetsService,
+        HttpErrorHandlerService,
+        // MessageService
+      ]
+    });
 
-    imports: [ HttpClientTestingModule ],
-    providers: [
-      TweetsService, HttpErrorHandlerService
-      // { provide: HttpErrorHandlerService, useValue: mockHttpErrorHandlerService }
-    ]
-  }));
-
-  it('should be created', () => {
-    const service: TweetsService = TestBed.inject(TweetsService);
-    expect(service).toBeTruthy();
+    // Inject the http, test controller, and service-under-test
+    // as they will be referenced by each test.
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    tweetsService = TestBed.inject(TweetsService);
   });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
+  });
+
+
+
+
+  describe('#getTweetDay', () => {
+    let expectedTweet: TweetDay;
+    let bird: BirdSummaryViewModel;
+
+    beforeEach(() => {
+      tweetsService = TestBed.inject(TweetsService);
+      expectedTweet = {
+        tweetDayId: 1, displayDay: 'Date | string', creationDate: 'Date | string',
+        lastUpdateDate: 'Date | string', bird: bird
+      };
+    });
+
+    it('should return expected TweetDay (called once)', () => {
+
+      tweetsService.getTweetDay().subscribe(
+        heroes => expect(heroes).toEqual(expectedTweet, 'should return expected heroes'),
+        fail
+      );
+
+      // TweetService should have made one request to GET heroes from expected URL
+      const req = httpTestingController.expectOne('api/Tweets/GetTweetDay');
+      expect(req.request.method).toEqual('GET');
+
+      // Respond with the mock tweet
+      req.flush(expectedTweet);
+    });
+
+  });
+
 });
