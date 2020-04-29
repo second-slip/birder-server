668465989a5452f1339e1200a6cbf8246b38faae
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2020-04-29 23:32:53 +0100

    user profile unit tests

--- Birder/ClientApp/src/app/_services/user-profile.service.spec.ts
+++ Birder/ClientApp/src/app/_services/user-profile.service.spec.ts
@@ -2,18 +2,90 @@ import { TestBed } from '@angular/core/testing';
 
 import { UserProfileService } from './user-profile.service';
 import { HttpErrorHandlerService } from './http-error-handler.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { UserProfileViewModel } from '@app/_models/UserProfileViewModel';
+import { ErrorReportViewModel } from '@app/_models/ErrorReportViewModel';
 
 describe('UserProfileService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [ HttpClientTestingModule ],
-    providers: [
-      UserProfileService, HttpErrorHandlerService
-    ]
-  }));
+  // let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let userProfileService: UserProfileService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      // Import the HttpClient mocking services
+      imports: [HttpClientTestingModule],
+      // Provide the service-under-test and its dependencies
+      providers: [
+        UserProfileService,
+        HttpErrorHandlerService,
+      ]
+    });
+
+    // Inject the http, test controller, and service-under-test
+    // as they will be referenced by each test.
+    // httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    userProfileService = TestBed.inject(UserProfileService);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
+  });
 
   it('should be created', () => {
     const service: UserProfileService = TestBed.inject(UserProfileService);
     expect(service).toBeTruthy();
   });
+
+  describe('#getUserProfile', () => {
+    let expectedProfile: UserProfileViewModel;
+    //let bird: BirdSummaryViewModel;
+
+    beforeEach(() => {
+      userProfileService = TestBed.inject(UserProfileService);
+      expectedProfile = {
+        avatar: '', isFollowing: false, isOwnProfile: false, registrationDate: '',
+        userName: '', followers: [], following: []
+      };
+    });
+
+    it('should return expected UserProfile (called once)', () => {
+      const username = 'andrew';
+
+      userProfileService.getUserProfile(username).subscribe(
+        heroes => expect(heroes).toEqual(expectedProfile, 'should return expected user profile'),
+        fail
+      );
+
+      // TweetService should have made one request to GET heroes from expected URL
+      const req = httpTestingController.expectOne(`api/UserProfile?requestedUsername=${username}`);
+      expect(req.request.method).toEqual('GET');
+
+      // Respond with the mock tweet
+      req.flush(expectedProfile);
+    });
+
+    it('should return ErrorReportViewModel if throws 404 error', () => {
+      const username = 'andrew';
+
+      userProfileService.getUserProfile(username).subscribe(
+        data => fail('Should have failed with 404 error'),
+        (error: ErrorReportViewModel) => {
+          expect(error.errorNumber).toEqual(404);
+          expect(error.message).toContain('Not Found');
+          expect(error.type).toContain('unsuccessful response code');
+          expect(error.friendlyMessage).toContain('An error occurred retrieving data.');
+        }
+      );
+
+      const req = httpTestingController.expectOne(`api/UserProfile?requestedUsername=${username}`);
+
+      // respond with a 404 and the error message in the body
+      const msg = 'deliberate 404 error';
+      req.flush(msg, {status: 404, statusText: 'Not Found'});
+    });
+  });
+
 });
--- Birder/ClientApp/src/app/_services/user-profile.service.ts
+++ Birder/ClientApp/src/app/_services/user-profile.service.ts
@@ -1,18 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpErrorHandlerService } from './http-error-handler.service';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { UserProfileViewModel } from '../_models/UserProfileViewModel';
 import { Observable } from 'rxjs';
-import { ErrorReportViewModel } from '../_models/ErrorReportViewModel';
 import { catchError } from 'rxjs/operators';
-
-// const httpOptions = {
-//   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-// };
+import { HttpErrorHandlerService } from './http-error-handler.service';
+import { UserProfileViewModel } from '@app/_models/UserProfileViewModel';
+import { ErrorReportViewModel } from '@app/_models/ErrorReportViewModel';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class UserProfileService {
 
   constructor(private http: HttpClient
