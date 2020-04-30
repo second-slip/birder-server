d8deb3372ba906ef61bfa20ef202dfe0f2735fa0
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2020-04-30 19:26:39 +0100

    validate username function

--- Birder/ClientApp/src/app/testing/testing.component.ts
+++ Birder/ClientApp/src/app/testing/testing.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormBuilder, FormControl, Validators, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, Validators, FormGroup, AbstractControl, ValidatorFn } from '@angular/forms';
 import { AccountService } from '@app/_services/account.service';
 import { AccountManagerService } from '@app/_services/account-manager.service';
 import { ManageProfileViewModel } from '@app/_models/ManageProfileViewModel';
 import { ErrorReportViewModel } from '@app/_models/ErrorReportViewModel';
+import { catchError, map } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-testing',
@@ -44,6 +46,10 @@ export class TestingComponent implements OnInit {
     this.getUserProfile();
   }
 
+  get name() { return this.manageProfileForm.get('username'); }
+
+  get email() { return this.manageProfileForm.get('email'); }
+
   createForms() {
 
     this.manageProfileForm = this.formBuilder.group({
@@ -51,10 +57,15 @@ export class TestingComponent implements OnInit {
         // ValidateEmailNotTaken.createValidator(this.accountService),
         // this.validateEmailNotTaken.bind(this),
         // UsernameValidator.validUsername,
+        // forbiddenNameValidator(/bob/),
+        // forbiddenNameValidator1(/bob/).bind(this),
         Validators.maxLength(25),
         Validators.minLength(5),
         Validators.pattern('^(?=.*[a-zA-Z])[a-zA-Z0-9]+$'), // ^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$
-        Validators.required
+        Validators.required,
+        this.validateUsername.bind(this)
+        // forbiddenNameValidator1.bind(this)
+        
       ])),
       email: new FormControl(this.user.email, Validators.compose([
         Validators.required,
@@ -76,4 +87,84 @@ export class TestingComponent implements OnInit {
         });
   }
 
+  private validateUsername(control: AbstractControl) {
+    const val = control.value;
+    return this.accountService.checkValidUsername(val) // httpService.validateUsername(val)
+    .subscribe(
+      (isAvailable: boolean) => {
+      console.log(isAvailable);
+      return isAvailable ? {'username': {value: control.value}} : null;
+        // const convertedName = res['user_name'];
+        // return convertedName === val ? { alreadyExist: true } : null;
+      }),
+      catchError(err => {
+        console.log(err.error);
+        console.log(err.message);
+        // this.messagesService.openDialog('Error aef-2', 'Database not available.');
+        return of({ "error": true })
+      })
+    // );
+  }
+
+  private forbiddenNameValidator1(): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: any} | null => {
+      // const forbidden = nameRe.test(control.value);
+      console.log(control.value)
+      return this.accountService.checkValidUsername(control.value) // httpService.validateUsername(val)
+      .subscribe((isAvailable: boolean) => {
+          // const convertedName = res['user_name'];
+          // return convertedName === val ? { alreadyExist: true } : null;
+          console.log(isAvailable);
+          return isAvailable ? {'username': {value: control.value}} : null;
+              // return forbidden ? {'forbiddenName': {value: control.value}} : null;
+      // returns either null if the control value is valid or a validation error object
+        })
+        // catchError(err => {
+        //   console.log(err.error);
+        //   console.log(err.message);
+        //   // this.messagesService.openDialog('Error aef-2', 'Database not available.');
+        //   return of({ 'username': true })
+        // });
+      // );
+      // return forbidden ? {'forbiddenName': {value: control.value}} : null;
+      // returns either null if the control value is valid or a validation error object
+    };
+  }
+
+  /** A hero's name can't match the given regular expression */
+
+
+}
+
+export function forbiddenNameValidator1(): ValidatorFn {
+  return (control: AbstractControl): {[key: string]: any} | null => {
+    // const forbidden = nameRe.test(control.value);
+    console.log(control.value)
+    return this.accountService.checkValidUsername(control.value) // httpService.validateUsername(val)
+    .subscribe((isAvailable: boolean) => {
+        // const convertedName = res['user_name'];
+        // return convertedName === val ? { alreadyExist: true } : null;
+        console.log(isAvailable);
+        return isAvailable ? {'username': {value: control.value}} : null;
+            // return forbidden ? {'forbiddenName': {value: control.value}} : null;
+    // returns either null if the control value is valid or a validation error object
+      })
+      // catchError(err => {
+      //   console.log(err.error);
+      //   console.log(err.message);
+      //   // this.messagesService.openDialog('Error aef-2', 'Database not available.');
+      //   return of({ 'username': true })
+      // });
+    // );
+    // return forbidden ? {'forbiddenName': {value: control.value}} : null;
+    // returns either null if the control value is valid or a validation error object
+  };
+}
+
+
+export function forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
+  return (control: AbstractControl): {[key: string]: any} | null => {
+    const forbidden = nameRe.test(control.value);
+    return forbidden ? {'forbiddenName': {value: control.value}} : null;
+  };
 }
