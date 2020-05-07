205e2b752f4d081428997d8eced36a49f4a3ec62
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2020-05-07 14:34:26 +0100

    GetUserWithNetworkAsync unit testing

---
+++ Birder.Tests/HelpersTests/ObservationsAnalysisHelperTests.cs
@@ -0,0 +1,183 @@
+﻿using Birder.Data.Model;
+using Birder.Helpers;
+using Birder.ViewModels;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Birder.Tests.HelpersTests
+{
+    public class ObservationsAnalysisHelperTests
+    {
+        #region MapLifeList
+
+        [Fact]
+        public void MapLifeList_ReturnsNullReferenceException_WhenArgumentIsNull()
+        {
+            // Act & Assert
+            var ex = Assert.Throws<ArgumentNullException>(() => ObservationsAnalysisHelper.MapLifeList(null));
+            Assert.Equal("The observations collection is null (Parameter 'observations')", ex.Message);
+        }
+
+        [Theory]
+        [InlineData(2, 2)]
+        [InlineData(5, 20)]
+        [InlineData(22, 7)]
+        [InlineData(8, 12)]
+        public void MapLifeList_ReturnsLifeListViewModelList_OnSuccess(int numberSpecies, int numberObs)
+        {
+            // Arrange
+            var birds = GetTestBirdsList(numberSpecies);
+            var testObservations = GetTestObservations(numberObs, birds);
+
+            // Act
+            var result = ObservationsAnalysisHelper.MapLifeList(testObservations);
+
+            // Assert
+            Assert.IsAssignableFrom<IEnumerable<LifeListViewModel>>(result);
+            Assert.Equal(numberSpecies, result.Count());
+            foreach(var record in result)
+            {
+                Assert.Equal(numberObs, record.Count);
+            }
+        }
+
+        [Fact]
+        public void MapLifeList_ReturnsLifeListViewModelList_OnSuccessfulNullNestedObject()
+        {
+            // Arrange
+            int numberSpecies = 3;
+            int numberObs = 3;
+            var birds = GetTestBirdsList(numberSpecies);
+            var testObservations = GetTestObservations(numberObs, birds);
+            foreach(var item in testObservations)
+            {
+                item.Bird.BirdConservationStatus = null;
+            }
+
+            // Act
+            var result = ObservationsAnalysisHelper.MapLifeList(testObservations);
+
+            // Assert
+            Assert.IsAssignableFrom<IEnumerable<LifeListViewModel>>(result);
+            Assert.Equal(numberSpecies, result.Count());
+            foreach (var item in result)
+            {
+                Assert.Equal(numberObs, item.Count);
+                Assert.Null(item.ConservationStatus);
+            }
+        }
+
+
+        #endregion
+
+
+        #region MapTopObservations
+
+        [Fact]
+        public void MapTopObservations_ReturnsNullReferenceException_WhenObservationsArgumentIsNull()
+        {
+            // Act & Assert
+            var ex = Assert.Throws<ArgumentNullException>(() => ObservationsAnalysisHelper.MapTopObservations(null, DateTime.Now));
+            Assert.Equal("The observations collection is null (Parameter 'observations')", ex.Message);
+        }
+
+        [Theory]
+        [InlineData(2, 2)]
+        [InlineData(5, 20)]
+        [InlineData(22, 7)]
+        [InlineData(8, 12)]
+        public void MapTopObservations_ReturnsLifeListViewModelList_OnSuccess(int numberSpecies, int numberObs)
+        {
+            // Arrange
+            var birds = GetTestBirdsList(numberSpecies);
+            var testObservations = GetTestObservations(numberObs, birds);
+
+            // Act
+            var result = ObservationsAnalysisHelper.MapTopObservations(testObservations, DateTime.Now);
+
+            // Assert
+            Assert.IsType<TopObservationsAnalysisViewModel>(result);
+            
+            foreach (var record in result.TopObservations)
+            {
+                var exp = testObservations.Where(x => x.BirdId == record.BirdId).Count();
+                Assert.Equal(exp, record.Count);
+            }
+
+            foreach (var record in result.TopMonthlyObservations)
+            {
+                var exp = testObservations.Where(x => x.BirdId == record.BirdId).Count();
+                Assert.Equal(exp, record.Count);
+            }
+        }
+
+        #endregion
+
+
+        private List<Bird> GetTestBirdsList(int length)
+        {
+            var conservationStatus = new ConservationStatus() { ConservationList = "Red" };
+            var birds = new List<Bird>();
+            
+            for (int i = 0; i < length; i++)
+            {
+                birds.Add(GetTestBird(i, conservationStatus));
+            };
+
+            return birds;
+        }
+
+        private Bird GetTestBird(int i, ConservationStatus cs)
+        {
+            return new Bird() 
+            { 
+                BirdId = i, 
+                EnglishName = "Name " + i.ToString(), 
+                Species = "Species " + i.ToString(),
+                PopulationSize = "Pop size",
+                BirdConservationStatus = cs 
+            };
+        }
+
+        private IEnumerable<Observation> GetTestObservations(int length, List<Bird> birds)
+        {
+            var observations = new List<Observation>();
+
+            for (int i = 0; i < birds.Count; i++)
+            {
+                var bird = birds[i];
+
+                for (int y = 0; y < length; y++)
+                {
+                    observations.Add(new Observation
+                    {
+                        ObservationId = y,
+                        LocationLatitude = 0,
+                        LocationLongitude = 0,
+                        Quantity = 1,
+                        NoteGeneral = "",
+                        NoteHabitat = "",
+                        NoteWeather = "",
+                        NoteAppearance = "",
+                        NoteBehaviour = "",
+                        NoteVocalisation = "",
+                        HasPhotos = false,
+                        SelectedPrivacyLevel = PrivacyLevel.Public,
+                        ObservationDateTime = DateTime.Now.AddDays(-4),
+                        CreationDate = DateTime.Now.AddDays(-4),
+                        LastUpdateDate = DateTime.Now.AddDays(-4),
+                        ApplicationUserId = "",
+                        BirdId = bird.BirdId,
+                        Bird = bird,
+                        ApplicationUser = null,
+                        ObservationTags = null
+                    });
+                }
+            }
+
+            return observations;
+        }
+    }
+}
---
+++ Birder.Tests/HelpersTests/UserManagerExtensionTests/GetUserWithNetworkAsyncTests.cs
@@ -0,0 +1,77 @@
+﻿using Birder.Data;
+using Birder.Helpers;
+using Birder.TestsHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestSupport.EfHelpers;
+using Xunit;
+using Xunit.Extensions.AssertExtensions;
+
+namespace Birder.Tests.HelpersTests
+{
+    public class GetUserWithNetworkAsyncTests
+    {
+
+        public GetUserWithNetworkAsyncTests()
+        {
+
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task GetUserWithNetworkAsync_WhenArgumentIsEmpty_ReturnsException()
+        {
+            var options = this.CreateUniqueClassOptions<ApplicationDbContext>();
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                // Arrange
+                context.CreateEmptyViaWipe();
+                context.Database.EnsureCreated();
+
+                var userManager = SharedFunctions.InitialiseUserManager(context);
+
+                // Act & Assert
+                var ex = await Assert.ThrowsAsync<ArgumentException>(() => userManager.GetUserWithNetworkAsync(string.Empty));
+                Assert.Equal("The argument is null or empty (Parameter 'username')", ex.Message);
+            }
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task GetUserWithNetworkAsync_()
+        {
+            var options = this.CreateUniqueClassOptions<ApplicationDbContext>();
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                // Arrange
+                context.CreateEmptyViaWipe();
+                context.Database.EnsureCreated();
+                //context.SeedDatabaseFourBooks();
+
+                //context.Users.Add(SharedFunctions.CreateUser("testUser1"));
+                //context.Users.Add(SharedFunctions.CreateUser("testUser2"));
+
+                //context.SaveChanges();
+
+                //context.Users.Count().ShouldEqual(0);
+
+                // Arrange
+                var userManager = SharedFunctions.InitialiseUserManager(context);
+
+
+
+                // Act & Assert
+                //var ex = Assert.Throws<ArgumentNullException>(() => ObservationsAnalysisHelper.MapLifeList(null));
+                var ex = await Assert.ThrowsAsync<ArgumentException>(() => userManager.GetUserWithNetworkAsync(string.Empty));
+                Assert.Equal("The argument is null or empty (Parameter 'username')", ex.Message);
+                //if (string.IsNullOrEmpty(username))
+                //    throw new ArgumentException("The argument is null or empty", nameof(username));
+            }
+
+        }
+
+
+    }
+}
