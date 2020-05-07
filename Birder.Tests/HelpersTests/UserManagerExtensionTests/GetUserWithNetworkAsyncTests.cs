babee8b9e38075af6a9f435aefcb7f78e39a547d
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2020-05-07 15:31:40 +0100

    GetUserWithNetworkAsync sucess unit test

--- Birder.Tests/HelpersTests/UserManagerExtensionTests/GetUserWithNetworkAsyncTests.cs
+++ Birder.Tests/HelpersTests/UserManagerExtensionTests/GetUserWithNetworkAsyncTests.cs
@@ -1,10 +1,10 @@
 ﻿using Birder.Data;
+using Birder.Data.Model;
 using Birder.Helpers;
 using Birder.TestsHelpers;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Threading.Tasks;
 using TestSupport.EfHelpers;
 using Xunit;
 using Xunit.Extensions.AssertExtensions;
@@ -19,8 +19,10 @@ namespace Birder.Tests.HelpersTests
 
         }
 
-        [Fact]
-        public async System.Threading.Tasks.Task GetUserWithNetworkAsync_WhenArgumentIsEmpty_ReturnsException()
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public async Task GetUserWithNetworkAsync_ReturnsException_WhenArgumentIsNullOrEmpty(string username)
         {
             var options = this.CreateUniqueClassOptions<ApplicationDbContext>();
 
@@ -33,45 +35,64 @@ namespace Birder.Tests.HelpersTests
                 var userManager = SharedFunctions.InitialiseUserManager(context);
 
                 // Act & Assert
-                var ex = await Assert.ThrowsAsync<ArgumentException>(() => userManager.GetUserWithNetworkAsync(string.Empty));
+                var ex = await Assert.ThrowsAsync<ArgumentException>(() => userManager.GetUserWithNetworkAsync(username));
                 Assert.Equal("The argument is null or empty (Parameter 'username')", ex.Message);
             }
         }
 
         [Fact]
-        public async System.Threading.Tasks.Task GetUserWithNetworkAsync_()
+        public async Task GetUserWithNetworkAsync_ReturnsUser_WithOneFollowerandFollowed()
         {
             var options = this.CreateUniqueClassOptions<ApplicationDbContext>();
 
             using (var context = new ApplicationDbContext(options))
             {
                 // Arrange
+                string usernameToAct = "User1";
+                string usernameToFollow = "User2";
+
                 context.CreateEmptyViaWipe();
                 context.Database.EnsureCreated();
                 //context.SeedDatabaseFourBooks();
 
-                //context.Users.Add(SharedFunctions.CreateUser("testUser1"));
-                //context.Users.Add(SharedFunctions.CreateUser("testUser2"));
+                context.Users.Add(SharedFunctions.CreateUser(usernameToAct));
+                context.Users.Add(SharedFunctions.CreateUser(usernameToFollow));
+                context.SaveChanges();
+                context.Users.Count().ShouldEqual(2);
 
-                //context.SaveChanges();
-
-                //context.Users.Count().ShouldEqual(0);
-
-                // Arrange
                 var userManager = SharedFunctions.InitialiseUserManager(context);
 
+                var userToAct = await userManager.FindByNameAsync(usernameToAct);
+                var userToFollow = await userManager.FindByNameAsync(usernameToFollow);
 
+                // userToAct follows userToFollow
+                context.Network.Add(new Network()
+                {
+                    ApplicationUser = userToFollow,
+                    Follower = userToAct,
+                });
 
-                // Act & Assert
-                //var ex = Assert.Throws<ArgumentNullException>(() => ObservationsAnalysisHelper.MapLifeList(null));
-                var ex = await Assert.ThrowsAsync<ArgumentException>(() => userManager.GetUserWithNetworkAsync(string.Empty));
-                Assert.Equal("The argument is null or empty (Parameter 'username')", ex.Message);
-                //if (string.IsNullOrEmpty(username))
-                //    throw new ArgumentException("The argument is null or empty", nameof(username));
+                // userToFollow follows userToAct
+                context.Network.Add(new Network()
+                {
+                    ApplicationUser = userToAct,
+                    Follower = userToFollow,
+                    //FollowerId = userToAct.Id
+                });
+
+                context.SaveChanges();
+                context.Network.Count().ShouldEqual(2);
+
+                // Act
+                var actual = await userManager.GetUserWithNetworkAsync(usernameToAct);
+
+                // Assert
+                actual.ShouldBeType<ApplicationUser>();
+                actual.Following.Count().ShouldEqual(1);
+                actual.Following.FirstOrDefault().ApplicationUser.UserName.ShouldEqual(usernameToFollow);
+                actual.Followers.Count().ShouldEqual(1);
+                actual.Followers.FirstOrDefault().Follower.UserName.ShouldEqual(usernameToFollow);
             }
-
         }
-
-
     }
 }
