f5c558d3474a67311b996d74f329970f796843c3
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2020-05-11 00:26:44 +0100

    network suggestions added

--- Birder/ClientApp/src/app/_network/network-suggestions/network-suggestions.component.ts
+++ Birder/ClientApp/src/app/_network/network-suggestions/network-suggestions.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NetworkUserViewModel } from '@app/_models/UserProfileViewModel';
+import { NetworkService } from '@app/_services/network.service';
+import { ToastrService } from 'ngx-toastr';
+import { ErrorReportViewModel } from '@app/_models/ErrorReportViewModel';
 
 @Component({
   selector: 'app-network-suggestions',
@@ -6,10 +10,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./network-suggestions.component.scss']
 })
 export class NetworkSuggestionsComponent implements OnInit {
+  users: NetworkUserViewModel[];
 
-  constructor() { }
+  constructor(private networkService: NetworkService
+    , private toast: ToastrService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getNetwork();
+  }
+
+  getNetwork(): void {
+    this.networkService.getNetworkSuggestions()
+      .subscribe(
+        (data: NetworkUserViewModel[]) => {
+          this.users = data;
+        },
+        (error: ErrorReportViewModel) => {
+          this.toast.error(error.serverCustomMessage, 'An error occurred');
+        });
+  }
+
+  followOrUnfollow(element, user: NetworkUserViewModel): void {
+    const action = element.innerText;
+
+    if (action === 'Follow') {
+      this.networkService.postFollowUser(user)
+        .subscribe(
+          (data: NetworkUserViewModel) => {
+            this.toast.info('You are now following ' + data.userName, 'Success');
+            element.innerText = 'Unfollow';
+          },
+          (error: ErrorReportViewModel) => {
+            this.toast.error(error.serverCustomMessage, 'An error occurred');
+          });
+      return;
+    } else {
+      this.networkService.postUnfollowUser(user)
+        .subscribe(
+          (data: NetworkUserViewModel) => {
+            this.toast.info('You have unfollowed ' + data.userName, 'Success');
+            element.innerText = 'Follow';
+          },
+          (error: ErrorReportViewModel) => {
+            console.log(error);
+            this.toast.error(error.serverCustomMessage, 'An error occurred');
+          });
+      return;
+    }
   }
 
 }
