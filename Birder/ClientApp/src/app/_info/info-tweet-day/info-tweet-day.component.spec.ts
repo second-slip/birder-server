9a90d690ba0d5d009c7bfd9005f8494ad492b339
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2020-05-13 14:01:49 +0100

    unit test added

--- Birder/ClientApp/src/app/_info/info-tweet-day/info-tweet-day.component.spec.ts
+++ Birder/ClientApp/src/app/_info/info-tweet-day/info-tweet-day.component.spec.ts
@@ -1,49 +1,77 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { InfoTweetDayComponent } from './info-tweet-day.component';
 import { TweetsService } from '@app/_services/tweets.service';
 import { of } from 'rxjs';
 import { TweetDay } from '@app/_models/TweetDay';
-
-
+import { BirdSummaryViewModel } from '@app/_models/BirdSummaryViewModel';
 
 describe('InfoTweetDayComponent', () => {
   let component: InfoTweetDayComponent;
   let fixture: ComponentFixture<InfoTweetDayComponent>;
 
   let mockTweetsService;
-  let heroes: TweetDay;
+  let mockTweet: TweetDay;
+  let mockBird: BirdSummaryViewModel;
 
   beforeEach(async(() => {
-    mockTweetsService  = jasmine.createSpyObj(['getTweetDay']);
+    mockTweetsService = jasmine.createSpyObj(['getTweetDay']);
 
     TestBed.configureTestingModule({
-      declarations: [ InfoTweetDayComponent ],
+      declarations: [InfoTweetDayComponent],
       providers: [
         { provide: TweetsService, useValue: mockTweetsService }
       ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
 
   beforeEach(() => {
     fixture = TestBed.createComponent(InfoTweetDayComponent);
     component = fixture.componentInstance;
-
-    let myObj = {tweetDayId: 11, songUrl: '', displayDay: new Date(Date.now()), creationDate: new Date(Date.now()),
-    lastUpdateDate: new Date(Date.now()),
-    bird: {birdId: 1, species: '', englishName: '', populationSize: '', btoStatusInBritain: '',
-     thumbnailUrl: '', conservationStatus: '', conservationListColourCode: '', birderStatus: '' } };
-    heroes = myObj;
-
-    mockTweetsService.getTweetDay.and.returnValue(of(heroes));
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('loads tweet on creation', () => {
+
+    // check DOM displays the correct ng-template...
+
+    it('should load tweet oninit', () => {
+      // Arrange
+      mockBird = {
+        birdId: 1,
+        species: 'string',
+        englishName: 'string',
+        populationSize: 'string',
+        btoStatusInBritain: 'string',
+        thumbnailUrl: 'string',
+        conservationStatus: 'string',
+        conservationListColourCode: 'string',
+        birderStatus: 'string'
+      };
+
+      mockTweet = {
+        tweetDayId: 1, songUrl: '', displayDay: new Date(Date.now()),
+        creationDate: new Date(Date.now()),
+        lastUpdateDate: new Date(Date.now()),
+        bird: mockBird
+      };
+
+      mockTweetsService.getTweetDay.and.returnValue(of(mockTweet));
+
+      // Act or change
+      fixture.detectChanges();
+
+      // Assert
+      expect(component).toBeTruthy();
+      expect(component.tweet).toEqual(mockTweet);
+    });
+
+  });
+
 });
 
 
