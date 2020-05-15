5d8ff8d3ce09f5d98971cd105f2678bde3edadff
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2020-05-15 13:19:29 +0100

    new test file added

---
+++ Birder.Tests/Services/ObservationsAnalysisServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Birder.Tests.Services
+{
+    public class ObservationsAnalysisServiceTests
+    {
+
+        public ObservationsAnalysisServiceTests() { }
+
+
+        [Fact]
+        public async Task a_b_c()
+        {
+            Assert.True(true);
+        }
+    }
+}
