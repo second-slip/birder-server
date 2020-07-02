4f69e1f88914746541cee11967fdfc2abf108e27
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2020-07-02 11:20:27 +0100

    adjusting styling

--- Birder/ClientApp/src/app/_sharedComponents/showcase-observations-list/showcase-observations-list.component.ts
+++ Birder/ClientApp/src/app/_sharedComponents/showcase-observations-list/showcase-observations-list.component.ts
@@ -12,7 +12,7 @@ import { ErrorReportViewModel } from '@app/_models/ErrorReportViewModel';
 })
 export class ShowcaseObservationsListComponent implements OnInit {
   observations: ObservationViewModel[];
-  private itemHeight = 177; //177
+  private itemHeight = 192; //177
 
   constructor(private observationsFeedService: ObservationsFeedService) { }
 
