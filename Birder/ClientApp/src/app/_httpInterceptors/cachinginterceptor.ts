bd7582f7c9100c1786f69e6deb0d756803c57a27
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2020-11-16 16:57:32 +0000

    do not cache GetObservation

--- Birder/ClientApp/src/app/_httpInterceptors/cachinginterceptor.ts
+++ Birder/ClientApp/src/app/_httpInterceptors/cachinginterceptor.ts
@@ -49,6 +49,7 @@ function isCachable(request: HttpRequest<any>) {
     && request.url.indexOf('api/Photograph') !== 0
     && request.url.indexOf('https://preview.ibb.co') !== 0
     && request.url.indexOf('api/ObservationFeed') !== 0
+    && request.url.indexOf('api/Observation/GetObservation') !== 0
     && request.url.indexOf('api/ObservationAnalysis') !== 0  // do not cache requests containing 'api/ObservationAnalysis'
     && request.url.indexOf('api/UserProfile') !== 0
     && request.url.indexOf('api/Network') !== 0
