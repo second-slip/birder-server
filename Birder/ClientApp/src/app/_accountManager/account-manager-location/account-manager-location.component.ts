2c33d87b6ce9e3151e21f073b8f2a2352c628049
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2020-11-16 21:50:55 +0000

    new map implementation

--- Birder/ClientApp/src/app/_accountManager/account-manager-location/account-manager-location.component.ts
+++ Birder/ClientApp/src/app/_accountManager/account-manager-location/account-manager-location.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ChangeDetectorRef, ViewEncapsulation } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { Component, OnInit, ChangeDetectorRef, ViewEncapsulation, ViewChild } from '@angular/core';
+// import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { SetLocationViewModel } from '@app/_models/SetLocationViewModel';
 import { ErrorReportViewModel } from '@app/_models/ErrorReportViewModel';
@@ -8,6 +8,7 @@ import { TokenService } from '@app/_services/token.service';
 import { AccountManagerService } from '@app/_services/account-manager.service';
 import { LocationViewModel } from '@app/_models/LocationViewModel';
 import { GeocodeService } from '@app/_services/geocode.service';
+import { GoogleMap, MapInfoWindow, MapMarker } from '@angular/google-maps';
 
 
 @Component({
@@ -18,18 +19,25 @@ import { GeocodeService } from '@app/_services/geocode.service';
 })
 export class AccountManagerLocationComponent implements OnInit {
   requesting: boolean;
-  model: SetLocationViewModel;
-  setLocationForm: FormGroup;
+  // model: SetLocationViewModel;
+  // setLocationForm: FormGroup;
   errorReport: ErrorReportViewModel;
   geolocation: string;
   searchAddress = '';
   geoError: string;
-  user: UserViewModel;
+  // user: UserViewModel;
+
+  @ViewChild(GoogleMap, { static: false }) map: GoogleMap
+  // @ViewChild(MapMarker, { static: false }) mark: MapMarker
+  @ViewChild(MapInfoWindow, { static: false }) infoWindow: MapInfoWindow
+  zoom = 11;
+  options: google.maps.MapOptions = {
+    mapTypeId: 'terrain'
+  }
 
   constructor(private router: Router
     , private tokenService: TokenService
     , private accountManager: AccountManagerService
-    , private formBuilder: FormBuilder
     , private geocodeService: GeocodeService
     , private ref: ChangeDetectorRef) { }
 
@@ -43,26 +51,53 @@ export class AccountManagerLocationComponent implements OnInit {
         (data: UserViewModel) => {
           // this.model.defaultLocationLatitude = data.defaultLocationLatitude;
           // this.model.defaultLocationLongitude = data.defaultLocationLongitude;
-          this.user = data;
-          this.createForms();
-          this.getGeolocation();
+          // this.user = data;
+          // this.createForms();
+          // this.getGeolocation();
+          this.addMarker(data.defaultLocationLatitude, data.defaultLocationLongitude);
         },
         (error: any) => {
           console.log('could not get the user, using default coordinates');
         });
   }
 
-  createForms(): void {
-    this.setLocationForm = this.formBuilder.group({
-      locationLatitude: new FormControl(this.user.defaultLocationLatitude),
-      locationLongitude: new FormControl(this.user.defaultLocationLongitude),
-    });
+  marker; // make marker a property?
+  addMarker(latitude: number, longitude:number) {
+    this.marker = ({
+      position: {
+        lat: latitude,
+        lng: longitude
+      },
+      label: {
+        color: 'red',
+        text: 'Marker label',
+      },
+      title: 'Marker title',
+      options: { draggable: true },
+    })
+
+    this.getGeolocation(latitude, longitude);
   }
+
+  openInfoWindow(marker: MapMarker) {
+    this.infoWindow.open(marker);
+  }
+
+  markerChanged(event: google.maps.MouseEvent): void {
+    this.addMarker(event.latLng.lat(), event.latLng.lng());
+  }
+
+  // createForms(): void {
+  //   this.setLocationForm = this.formBuilder.group({
+  //     locationLatitude: new FormControl(this.user.defaultLocationLatitude),
+  //     locationLongitude: new FormControl(this.user.defaultLocationLongitude),
+  //   });
+  // }
 
   // new google maps methods...
 
-  getGeolocation(): void {
-    this.geocodeService.reverseGeocode(this.user.defaultLocationLatitude, this.user.defaultLocationLongitude)
+  getGeolocation(latitude: number, longitude:number): void {
+    this.geocodeService.reverseGeocode(latitude, longitude)
       .subscribe(
         (data: LocationViewModel) => {
           this.geolocation = data.formattedAddress;
@@ -77,8 +112,10 @@ export class AccountManagerLocationComponent implements OnInit {
   useGeolocation(searchValue: string) {
     this.geocodeService.geocodeAddress(searchValue)
       .subscribe((location: LocationViewModel) => {
-        this.setLocationForm.get('locationLatitude').setValue(location.latitude);
-        this.setLocationForm.get('locationLongitude').setValue(location.longitude);
+        console.log(searchValue);
+        // this.setLocationForm.get('locationLatitude').setValue(location.latitude);
+        // this.setLocationForm.get('locationLongitude').setValue(location.longitude);
+        this.addMarker(location.latitude, location.longitude);
         this.geolocation = location.formattedAddress;
         this.searchAddress = '';
         this.ref.detectChanges();
@@ -115,25 +152,27 @@ export class AccountManagerLocationComponent implements OnInit {
     }
   }
 
-  placeMarker($event) {
-    this.geocodeService.reverseGeocode($event.coords.lat, $event.coords.lng)
-      .subscribe(
-        (location: LocationViewModel) => {
-          this.setLocationForm.get('locationLatitude').setValue(location.latitude);
-          this.setLocationForm.get('locationLongitude').setValue(location.longitude);
-          this.geolocation = location.formattedAddress;
-          this.ref.detectChanges();
-        },
-        (error: any) => { }
-      );
-  }
+  // placeMarker($event) {
+  //   this.geocodeService.reverseGeocode($event.coords.lat, $event.coords.lng)
+  //     .subscribe(
+  //       (location: LocationViewModel) => {
+  //         this.setLocationForm.get('locationLatitude').setValue(location.latitude);
+  //         this.setLocationForm.get('locationLongitude').setValue(location.longitude);
+  //         this.geolocation = location.formattedAddress;
+  //         this.ref.detectChanges();
+  //       },
+  //       (error: any) => { }
+  //     );
+  // }
 
-  onSubmit(value): void {
+  onSubmit(): void {
     this.requesting = true;
     const model = <SetLocationViewModel> {
-      defaultLocationLatitude: value.locationLatitude,
-      defaultLocationLongitude: value.locationLongitude,
+      defaultLocationLatitude: this.marker.position.lat,
+      defaultLocationLongitude: this.marker.position.lng,
     };
+
+    console.log(model);
 
     this.accountManager.postSetLocation(model)
     .subscribe(
