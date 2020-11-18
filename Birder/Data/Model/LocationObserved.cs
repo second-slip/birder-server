9030320c6b6552717df3dd3ef7800010655142b3
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2020-11-18 17:33:52 +0000

    ObservationPosition data model added

---
+++ Birder/Data/Model/LocationObserved.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Birder.Data.Model
+{
+    public class ObservationPosition
+    {
+        [Key]
+        public int ObservationPositionId { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Geolocation { get; set; }
+    }
+}
