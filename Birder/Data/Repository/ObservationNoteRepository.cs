d5f5441b8bf75bb5e37e1d782353f34b3532beff
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2020-11-19 16:18:08 +0000

    ObservationNoteRepository

---
+++ Birder/Data/Repository/ObservationNoteRepository.cs
@@ -0,0 +1,13 @@
+﻿using Birder.Data.Model;
+
+namespace Birder.Data.Repository
+{
+    public interface IObservationNoteRepository : IRepository<ObservationNote> { }
+
+    public class ObservationNoteRepository : Repository<ObservationNote>, IObservationNoteRepository
+    {
+        public ObservationNoteRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
