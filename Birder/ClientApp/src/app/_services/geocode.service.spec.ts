f92f4d88792188e62a8944e1da83572161b02128
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2020-11-24 12:28:27 +0000

    removing obsolete service unit tests

--- Birder/ClientApp/src/app/_services/geocode.service.spec.ts
+++ Birder/ClientApp/src/app/_services/geocode.service.spec.ts
@@ -1,18 +1,18 @@
-import { TestBed } from '@angular/core/testing';
+// import { TestBed } from '@angular/core/testing';
 
-import { GeocodeService } from './geocode.service';
-import { MapsAPILoader } from '@agm/core';
+// import { GeocodeService } from './geocode.service';
+// import { MapsAPILoader } from '@agm/core';
 
-describe('GeocodeService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    // imports: [ MapsAPILoader ],
-    providers: [
-      GeocodeService, MapsAPILoader
-    ]
-  }));
+// describe('GeocodeService', () => {
+//   beforeEach(() => TestBed.configureTestingModule({
+//     // imports: [ MapsAPILoader ],
+//     providers: [
+//       GeocodeService, MapsAPILoader
+//     ]
+//   }));
 
-  it('should be created', () => {
-    const service: GeocodeService = TestBed.inject(GeocodeService);
-    expect(service).toBeTruthy();
-  });
-});
+//   it('should be created', () => {
+//     const service: GeocodeService = TestBed.inject(GeocodeService);
+//     expect(service).toBeTruthy();
+//   });
+// });
