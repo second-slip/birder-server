d52e5a14a0f590506a8f5f113b310cd7023b9351
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2020-11-25 16:27:19 +0000

    test add notes

--- Birder/ClientApp/src/app/testing/testing.component.ts
+++ Birder/ClientApp/src/app/testing/testing.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { GeocodingService } from '@app/_services/geocoding.service';
 
 @Component({
   selector: 'app-testing',
@@ -8,38 +7,13 @@ import { GeocodingService } from '@app/_services/geocoding.service';
   encapsulation: ViewEncapsulation.None
 })
 export class TestingComponent implements OnInit {
-  formattedAddress: string;
-  formattedAddress2: string;
 
-  constructor(private geo: GeocodingService) { }
+
+  constructor() { }
 
   ngOnInit() {
-    this.findLocation();
-    this.findLocation2();
+
   }
 
-  findLocation(): void {
-    this.geo.geocode(`83 christchurch road, reading`)
-      .subscribe(
-        (response: any) => {
-          console.log(response.results[0]);
-          this.formattedAddress = response.results[0].formatted_address;
-          // coordinates
-        }),
-      ((error: any) => {
-        console.log(error);
-      });
-  }
 
-  findLocation2(): void {
-    this.geo.reverseGeocode(40.714224, -73.961452)
-      .subscribe(
-        (response: any) => {
-          console.log(response.results[0]);
-          this.formattedAddress2 = response.results[0].formatted_address;
-        }),
-      ((error: any) => {
-        console.log(error);
-      });
-  }
 }
