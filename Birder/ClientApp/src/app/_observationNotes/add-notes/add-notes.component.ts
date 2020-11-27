1f35134ecef8e99665f6594079db1b56e2aeae71
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2020-11-27 00:44:45 +0000

    form setup

--- Birder/ClientApp/src/app/_observationNotes/add-notes/add-notes.component.ts
+++ Birder/ClientApp/src/app/_observationNotes/add-notes/add-notes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ObservationNote } from '@app/_models/ObservationNote';
+import { ObservationNote, ObservationNoteType } from '@app/_models/ObservationNote';
 
 @Component({
   selector: 'app-add-notes',
@@ -8,6 +8,14 @@ import { ObservationNote } from '@app/_models/ObservationNote';
 })
 export class AddNotesComponent implements OnInit {
   notes: ObservationNote[] = [];
+  //
+  // model: ObservationNote;
+  keys = Object.keys;
+  keys1() : Array<string> { var keys = Object.keys(this.powers); return keys.slice(keys.length / 2); }
+  powers = ObservationNoteType; // ['Really Smart', 'Super Flexible',
+  //'Super Hot', 'Weather Changer'];
+
+  model = new NoteModel(ObservationNoteType.General, ''); // new Hero(0, '', this.powers[0], '');
 
   constructor() { }
 
@@ -25,5 +33,65 @@ export class AddNotesComponent implements OnInit {
 
 
 
+  submitted = false;
 
+  onSubmit() { 
+    // this.submitted = true; 
+    console.log(this.keys1);
+    console.log(this.model);
+    this.model = new NoteModel(0, '');
+  }
+
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model); }
+
+  // newHero() {
+  //   this.model = new NoteModel(ObservationNoteType.General, '');
+  // }
+
+  // skyDog(): Hero {
+  //   const myHero =  new Hero(42, 'SkyDog',
+  //                          'Fetch any object at any distance',
+  //                          'Leslie Rollover');
+  //   console.log('My hero is called ' + myHero.name); // "My hero is called SkyDog"
+  //   return myHero;
+  // }
+
+  //////// NOT SHOWN IN DOCS ////////
+
+  // Reveal in html:
+  //   Name via form.controls = {{showFormControls(heroForm)}}
+  showFormControls(form: any) {
+    return form && form.controls.note &&
+    form.controls.note.value; // Dr. IQ
+  }
+
+  /////////////////////////////
+
+}
+
+export class NoteModel {
+  constructor(
+    // public id: number,
+    public noteType: ObservationNoteType,
+    public note: string,
+  ) {  }
+
+}
+
+// export interface ObservationNote {
+//   id: number;
+//   noteType: ObservationNoteType; ///????
+//   note: string;
+//   obervationId: number;
+//   // observation: ObservationViewModel; ????
+// }
+
+export class Hero {
+  constructor(
+    public id: number,
+    public name: string,
+    public power: string,
+    public alterEgo?: string
+  ) {  }
 }
