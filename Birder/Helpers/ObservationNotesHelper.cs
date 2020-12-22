7ad68aac7a832d73bd90dc4d79058f63b8fe95d6
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2020-12-22 14:51:10 +0000

    guard added

--- Birder/Helpers/ObservationNotesHelper.cs
+++ Birder/Helpers/ObservationNotesHelper.cs
@@ -25,18 +25,15 @@ namespace Birder.Helpers
 
         public static IEnumerable<ObservationNoteDto> GetNewNotes(List<ObservationNoteDto> notes)
         {
-
+            if (notes is null)
+            {
+                throw new NullReferenceException("The notes collection is null");
+            }
 
             return notes.Where(i => i.Id == 0);
 
-
-
-
-
-
             //CreateMap<ObservationNoteDto, ObservationNote>()
             //    .ForMember(a => a.Observation, b => b.Ignore());
-
         }
     }
 }
