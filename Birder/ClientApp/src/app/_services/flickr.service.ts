960ec09bd9f3d6aec2ac33891224a2e5841eb237
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2021-01-19 16:49:30 +0000

    use api key from token

--- Birder/ClientApp/src/app/_services/flickr.service.ts
+++ Birder/ClientApp/src/app/_services/flickr.service.ts
@@ -3,17 +3,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'environments/environment';
 import { FlickrUrlsViewModel } from '../_models/FlickrUrlsViewModel';
+import { TokenService } from './token.service';
+import { tokenGetter } from '@app/app.module';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FlickrService {
-  private readonly apiKey = environment.flickrApiKey;
+  private readonly apiKey = this.token.getFlikrKey();
   private readonly apiUrl = 'https://api.flickr.com/services/rest/';
   private readonly baseUrl = `${this.apiUrl}?api_key=${this.apiKey}&format=json&nojsoncallback=1&method=flickr.photos.`;
   private readonly flickrPhotoSearch = `${this.baseUrl}search&per_page=20&tags=`;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private token: TokenService) { }
 
   getSearchResults(page: number, term = null): Observable<{}> {
     return this.getFlickrPhotoSearch(term, page, '');
