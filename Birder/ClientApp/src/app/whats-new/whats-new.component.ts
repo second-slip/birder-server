1f2b6ede87f12f7aaba416d1f7849196c98c554e
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2021-01-23 14:13:01 +0000

    colour code added

--- Birder/ClientApp/src/app/whats-new/whats-new.component.ts
+++ Birder/ClientApp/src/app/whats-new/whats-new.component.ts
@@ -16,7 +16,7 @@ export class WhatsNewComponent implements OnInit {
   ngOnInit(): void {
     this.getJSON().subscribe(data => {
       this.features = data;
-      // console.log(data);
+      console.log(data);
     });
   }
 
@@ -32,4 +32,5 @@ export interface IFeatures {
   description: string;
   progress: string;
   priority: string;
+  colourCode: string;
 }
