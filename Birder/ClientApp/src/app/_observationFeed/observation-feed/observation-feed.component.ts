5674dcd555897333b5119d5950dbbbabaad37eab
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2021-01-27 18:41:08 +0000

    fix sizing on different screen sizes

--- Birder/ClientApp/src/app/_observationFeed/observation-feed/observation-feed.component.ts
+++ Birder/ClientApp/src/app/_observationFeed/observation-feed/observation-feed.component.ts
@@ -25,7 +25,7 @@ export class ObservationFeedComponent implements OnInit {
   allLoaded = false;
   private cache = [];
   private pageByManual$ = new BehaviorSubject(1);
-  private itemHeight = 158;
+  private itemHeight = 165;
   private numberOfItems = 10;
   private pageByScroll$ = fromEvent(window, 'scroll')
     .pipe(
