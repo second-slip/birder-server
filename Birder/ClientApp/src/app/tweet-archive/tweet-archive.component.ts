ad8c21c53ff92dc6bff4b75f551645218d75fd47
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2021-01-29 16:19:11 +0000

    page archived tweets

--- Birder/ClientApp/src/app/tweet-archive/tweet-archive.component.ts
+++ Birder/ClientApp/src/app/tweet-archive/tweet-archive.component.ts
@@ -11,23 +11,31 @@ import { TweetsService } from '@app/_services/tweets.service';
 })
 export class TweetArchiveComponent implements OnInit {
   tweets: TweetArchiveDto;
+  totalItems: number;
+  page = 1;
+  pageSize = 25;
 
   constructor(private tweetsService: TweetsService) { }
 
   ngOnInit(): void {
-    this.getTweetArchive();
+    this.getTweetArchive(this.page, this.pageSize);
   }
 
-  getTweetArchive() {
-    this.tweetsService.getTweetArchive(1, 25)
+  getTweetArchive(page: number, pageSize: number) {
+    this.tweetsService.getTweetArchive(this.page, this.pageSize)
       .subscribe(
         (data: TweetArchiveDto) => {
           this.tweets = data;
+          this.totalItems = data.totalItems;
         },
         (error: ErrorReportViewModel) => {
           // TOAST........................................................................................
         }
       );
+  }
+
+  changePage() { // event) { // page: number) {
+    this.getTweetArchive(this.page, this.pageSize);
   }
 }
 
