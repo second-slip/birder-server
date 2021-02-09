114deb76676d32d05ff760b2a567f0a7ce12ea66
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2021-02-09 23:46:39 +0000

    Recordingconstroller scaffold

---
+++ Birder/Controllers/RecordingController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Birder.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = "Bearer")]
+    public class RecordingController : Controller
+    {
+        private readonly ILogger _logger;
+        public RecordingController() { }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
