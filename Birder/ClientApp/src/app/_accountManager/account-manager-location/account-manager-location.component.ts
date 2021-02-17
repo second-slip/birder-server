2ab0ba1c00313f02220e84789f74f932365a5c8f
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2021-02-17 11:47:09 +0000

    Fix: profile default location use shared map comp

--- Birder/ClientApp/src/app/_accountManager/account-manager-location/account-manager-location.component.ts
+++ Birder/ClientApp/src/app/_accountManager/account-manager-location/account-manager-location.component.ts
@@ -5,8 +5,8 @@ import { ErrorReportViewModel } from '@app/_models/ErrorReportViewModel';
 import { UserViewModel } from '@app/_models/UserViewModel';
 import { TokenService } from '@app/_services/token.service';
 import { AccountManagerService } from '@app/_services/account-manager.service';
-import { GoogleMap, MapInfoWindow, MapMarker } from '@angular/google-maps';
-import { GeocodingService } from '@app/_services/geocoding.service';
+import { ObservationPosition } from '@app/_models/ObservationPosition';
+import { ViewEditSingleMarkerMapComponent } from '@app/_maps/view-edit-single-marker-map/view-edit-single-marker-map.component';
 
 @Component({
   selector: 'app-account-manager-location',
@@ -15,24 +15,14 @@ import { GeocodingService } from '@app/_services/geocoding.service';
   encapsulation: ViewEncapsulation.None
 })
 export class AccountManagerLocationComponent implements OnInit {
+  @ViewChild(ViewEditSingleMarkerMapComponent)
+  private mapComponent: ViewEditSingleMarkerMapComponent;
   requesting: boolean;
-  errorReport: ErrorReportViewModel;
-  geolocation: string;
-  searchAddress = '';
-  geoError: string;
-  marker;
-
-  @ViewChild(GoogleMap, { static: false }) map: GoogleMap
-  @ViewChild(MapInfoWindow, { static: false }) infoWindow: MapInfoWindow
-  zoom = 11;
-  options: google.maps.MapOptions = {
-    mapTypeId: 'terrain'
-  }
+  defaultPosition: ObservationPosition;
 
   constructor(private router: Router
     , private tokenService: TokenService
     , private accountManager: AccountManagerService
-    , private geocoding: GeocodingService
     , private ref: ChangeDetectorRef) { }
 
   ngOnInit() {
@@ -43,105 +33,34 @@ export class AccountManagerLocationComponent implements OnInit {
     this.tokenService.getAuthenticatedUserDetails()
       .subscribe(
         (data: UserViewModel) => {
-          this.addMarker(data.defaultLocationLatitude, data.defaultLocationLongitude, true);
+          this.defaultPosition = <ObservationPosition>{
+            latitude: data.defaultLocationLatitude,
+            longitude: data.defaultLocationLongitude,
+            formattedAddress: '',
+            shortAddress: ''
+          };
         },
         (error: any) => {
-          console.log('could not get the user, using default coordinates');
+          this.defaultPosition = <ObservationPosition>{
+            latitude: 54.972237,
+            longitude: -2.4608560000000352,
+            formattedAddress: '',
+            shortAddress: ''
+          };
         });
-  }
-
-  addMarker(latitude: number, longitude: number, getAddress: boolean) {
-    this.marker = ({
-      position: {
-        lat: latitude,
-        lng: longitude
-      },
-      label: {
-        color: 'red',
-        text: 'Marker label',
-      },
-      title: 'Marker title',
-      options: { draggable: true },
-    })
-
-    if (getAddress) {
-      this.getFormattedAddress(latitude, longitude);
-    }
-  }
-
-  openInfoWindow(marker: MapMarker) {
-    this.infoWindow.open(marker);
-  }
-
-  markerChanged(event: google.maps.MouseEvent): void {
-    this.addMarker(event.latLng.lat(), event.latLng.lng(), true);
-  }
-
-  getFormattedAddress(latitude: number, longitude: number): void {
-    this.geocoding.reverseGeocode(latitude, longitude)
-      .subscribe(
-        (response: any) => {
-          this.geolocation = response.results[0].formatted_address;
-          this.ref.detectChanges();
-        },
-        (error: any) => {
-          //
-        }
-      );
-  }
-
-  findAddress(searchValue: string) {
-    this.geocoding.geocode(searchValue)
-      .subscribe(
-        (response: any) => {
-          this.addMarker(response.results[0].geometry.location.lat, response.results[0].geometry.location.lng, false); // false to stop second hit on API to get address...
-          this.geolocation = response.results[0].formatted_address;
-          this.searchAddress = '';
-          this.ref.detectChanges();
-        }
-      );
-  }
-
-  closeAlert(): void {
-    this.geoError = null;
-  }
-
-  getCurrentPosition(): void {
-    if (window.navigator.geolocation) {
-      window.navigator.geolocation.getCurrentPosition(
-        (position) => {
-          this.addMarker(position.coords.latitude, position.coords.longitude, true);
-        }, (error) => {
-          switch (error.code) {
-            case 3: // ...deal with timeout
-              this.geoError = 'The request to get user location timed out...';
-              break;
-            case 2: // ...device can't get data
-              this.geoError = 'Location information is unavailable...';
-              break;
-            case 1: // ...user said no ☹️
-              this.geoError = 'User denied the request for Geolocation...';
-              break;
-            default:
-              this.geoError = 'An error occurred with Geolocation...';
-          }
-        });
-    } else {
-      this.geoError = 'Geolocation not supported in this browser';
-    }
   }
 
   onSubmit(): void {
     this.requesting = true;
+
     const model = <SetLocationViewModel>{
-      defaultLocationLatitude: this.marker.position.lat,
-      defaultLocationLongitude: this.marker.position.lng,
+      defaultLocationLatitude: this.mapComponent.locationMarker.position.lat,
+      defaultLocationLongitude: this.mapComponent.locationMarker.position.lng,
     };
 
     this.accountManager.postSetLocation(model)
       .subscribe(
         (data: SetLocationViewModel) => {
-          //  console.log('successful registration');
           this.router.navigate(['login']);
         },
         (error: ErrorReportViewModel) => {
