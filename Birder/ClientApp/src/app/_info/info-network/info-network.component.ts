3a2ec5413e939f9dc93fce9706f2dc86b0fa3fe2
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2021-02-24 16:42:56 +0000

    update with shared component

--- Birder/ClientApp/src/app/_info/info-network/info-network.component.ts
+++ Birder/ClientApp/src/app/_info/info-network/info-network.component.ts
@@ -12,7 +12,6 @@ import { Subscription } from 'rxjs';
   encapsulation: ViewEncapsulation.None
 })
 export class InfoNetworkComponent implements OnInit, OnDestroy {
-  // user: UserProfileViewModel;
   network: UserNetworkDto;
   networkChangeSubscription: Subscription;
   requesting: boolean
@@ -42,39 +41,11 @@ export class InfoNetworkComponent implements OnInit, OnDestroy {
       .subscribe(
         (data: UserNetworkDto) => {
           this.network = data;
-          this.requesting = false;
         },
         (error: ErrorReportViewModel) => {
-          this.requesting = false;
           console.log(error);
-        });
-  }
-
-  followOrUnfollow(element, user: NetworkUserViewModel): void {
-    const action = element.innerText;
-
-    if (action === 'Follow') {
-      this.networkService.postFollowUser(user)
-        .subscribe(
-          (data: NetworkUserViewModel) => {
-            // this.getUser(); // obsolete due to event subsciption
-            // element.innerText = 'Unfollow';
-          },
-          (error: ErrorReportViewModel) => {
-            console.log(error);
-          });
-      return;
-    } else {
-      this.networkService.postUnfollowUser(user)
-        .subscribe(
-          (data: NetworkUserViewModel) => {
-            // this.getUser(); // obsolete due to event subsciption
-            // element.innerText = 'Follow';
-          },
-          (error: ErrorReportViewModel) => {
-            console.log(error);
-          });
-      return;
-    }
+        },
+        () => this.requesting = false
+        );
   }
 }
