54c3e1f675b683c7dc6bb48df1eae8333aa5be76
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2021-05-15 00:34:44 +0100

    adding sync pipe setup

--- Birder/ClientApp/src/app/_network/network-search/network-search.component.ts
+++ Birder/ClientApp/src/app/_network/network-search/network-search.component.ts
@@ -1,9 +1,8 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
 import { NetworkUserViewModel } from '@app/_models/UserProfileViewModel';
 import { NetworkService } from '@app/_network/network.service';
-import { ErrorReportViewModel } from '@app/_models/ErrorReportViewModel';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, share } from 'rxjs/operators';
 
 @Component({
   selector: 'app-network-search',
@@ -12,36 +11,18 @@ import { Observable } from 'rxjs';
   encapsulation: ViewEncapsulation.None
 })
 export class NetworkSearchComponent {
-  users: NetworkUserViewModel[];
-  //users$: Observable<NetworkUserViewModel[]>;
-  // customSearch = false;
-  searching = false;
+  results$: Observable<NetworkUserViewModel[]>;
+  public errorObject = null;
 
-  constructor(private networkService: NetworkService
-    , private toast: ToastrService) { }
+  constructor(private networkService: NetworkService) { }
 
   searchNetwork(value: any): void {
-    // this.customSearch = true;
-    this.searching = true;
-
-    this.networkService.getSearchNetwork(value.searchTerm)
-      .subscribe(
-        (data: NetworkUserViewModel[]) => {
-          this.users = data;
-          if (this.users.length > 0) {
-            this.toast.info(this.users.length.toString() + ' results were found', 'Search successful');
-          } else {
-            this.toast.warning('No results were found', 'Search unsuccessful');
-          }
-          this.searching = false;
-        },
-        (error: ErrorReportViewModel) => {
-          if (error.type === 'client-side or network error occurred') {
-            this.toast.error(error.serverCustomMessage, 'An error occurred');
-          } else {
-            this.toast.error('Try a different search query', 'Search unsuccessful');
-          }
-          this.searching = false;
-        });
+    this.results$ = this.networkService.getSearchNetwork(value.searchTerm)
+      .pipe(share(),
+        catchError(err => {
+          this.errorObject = err;
+          return throwError(err);
+        })
+      );
   }
 }
