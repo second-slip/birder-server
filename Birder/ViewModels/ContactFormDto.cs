23c1ca13e7692d6bbd1d6d2b9f9dbec189c10f16
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2021-06-15 20:36:20 +0100

    Obsolete DataAttributes removed (legacy from Razor)

--- Birder/ViewModels/ContactFormDto.cs
+++ Birder/ViewModels/ContactFormDto.cs
@@ -7,7 +7,7 @@ namespace Birder.ViewModels
         [Required]
         public string Name { get; set; }
         [Required]
-        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
         public string Email { get; set; }
         [Required]
         public string Message { get; set; }
--- Birder/ViewModels/LoginViewModel.cs
+++ Birder/ViewModels/LoginViewModel.cs
@@ -10,7 +10,6 @@ namespace Birder.ViewModels
         //public string Email { get; set; }
 
         [Required]
-        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         public bool RememberMe { get; set; }
