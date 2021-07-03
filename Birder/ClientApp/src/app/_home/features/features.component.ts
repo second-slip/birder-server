c9f14be676f86abf6b0871b92f8c8cf603b65571
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2021-07-03 14:52:14 +0100

    cosmetic changes

--- Birder/ClientApp/src/app/_home/features/features.component.ts
+++ Birder/ClientApp/src/app/_home/features/features.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-features',
@@ -6,4 +6,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./features.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class FeaturesComponent {}
+export class FeaturesComponent {
+  @Input()
+  public showBlurb: boolean;
+}
