fd8f334cb69807ca9af95caa4a00e9fc0efd8a2f
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2021-07-19 23:59:24 +0100

    exp

--- Birder/ClientApp/src/app/whats-new/whats-new.component.ts
+++ Birder/ClientApp/src/app/whats-new/whats-new.component.ts
@@ -1,6 +1,6 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
-import { catchError, share } from 'rxjs/operators';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Observable, Subject, throwError } from 'rxjs';
+import { catchError, share, take, takeUntil } from 'rxjs/operators';
 import { FeaturesService } from './features.service';
 
 @Component({
@@ -9,16 +9,38 @@ import { FeaturesService } from './features.service';
   styleUrls: ['./whats-new.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class WhatsNewComponent {
+export class WhatsNewComponent implements OnInit {
   features$: Observable<IFeatures[]>;
+  private ngUnsubscribe = new Subject();
+  public features: Subject<Array<IFeatures>> = new Subject();
   public errorObject = null;
 
-  constructor(private service: FeaturesService) {
-    this.features$ = this.getFeatures();
+  constructor(private readonly _service: FeaturesService) {
+    this.features$ = this._getFeatures();
+    // this._getFeatures();
   }
 
-  private getFeatures(): Observable<IFeatures[]> {
-    return this.service.getFeatures()
+  ngOnInit(): void {
+    this._getFeatures();
+  }
+
+  // private async _getFeatures(): Promise<void> {
+  //   this._service.getFeatures()
+  //     .pipe(take(1),takeUntil(this.ngUnsubscribe),
+  //       catchError(err => {
+  //         this.errorObject = err;
+  //         return throwError(err);
+  //       })
+  //     ).subscribe(result => { this.features.next(result) });
+  // }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+}
+
+  private _getFeatures(): Observable<IFeatures[]> {
+    return this._service.getFeatures()
       .pipe(share(),
         catchError(err => {
           this.errorObject = err;
