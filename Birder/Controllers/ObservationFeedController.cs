af1d554336b89cbd8dcbe2992ac59b6cf781fdc1
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2022-02-27 00:22:03 +0000

    remove obsolete code

--- Birder/Controllers/ObservationFeedController.cs
+++ Birder/Controllers/ObservationFeedController.cs
@@ -7,68 +7,24 @@ namespace Birder.Controllers;
 [Authorize(AuthenticationSchemes = "Bearer")]
 public class ObservationFeedController : ControllerBase
 {
-    // private const int pageSize = 10;
     private readonly ILogger _logger;
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly IObservationQueryService _observationQueryService;
-    // private readonly IBirdThumbnailPhotoService _profilePhotosService;
 
     public ObservationFeedController(ILogger<ObservationFeedController> logger
                                    , UserManager<ApplicationUser> userManager
                                    , IObservationQueryService observationQueryService)
-    //    , IBirdThumbnailPhotoService profilePhotosService)
     {
         _logger = logger;
         _userManager = userManager;
-        // _profilePhotosService = profilePhotosService;
         _observationQueryService = observationQueryService;
     }
 
     [HttpGet]
-    public async Task<IActionResult> GetObservationsFeedAsync(int pageIndex, int pageSize, ObservationFeedFilter filter)
+    public async Task<IActionResult> GetPublicFeedAsync(int pageIndex, int pageSize)
     {
         try
         {
-            /*
-             * if network observations is requested, or
-             * if user's own observations is request BUT user has no observations
-            */
-            if (filter == ObservationFeedFilter.Network || filter == ObservationFeedFilter.Own)
-            {
-                var requestingUserAndNetwork = await _userManager.GetUserWithNetworkAsync(User.Identity.Name);
-
-                if (requestingUserAndNetwork is null)
-                {
-                    _logger.LogWarning(LoggingEvents.GetItemNotFound, "Requesting user not found");
-                    return StatusCode(500, $"requesting user not found");
-                }
-
-                var followingUsernamesList = UserNetworkHelpers.GetFollowingUserNames(requestingUserAndNetwork.Following);
-
-                followingUsernamesList.Add(requestingUserAndNetwork.UserName);
-
-                var networkObservations = await _observationQueryService.GetPagedObservationsFeedAsync(o => followingUsernamesList.Contains(o.ApplicationUser.UserName), pageIndex, pageSize);
-
-                if (networkObservations is null)
-                {
-                    _logger.LogWarning(LoggingEvents.GetListNotFound, "Network observations list is null");
-                    return StatusCode(500, $"Network observations object is null");
-                }
-
-                if (networkObservations.TotalItems > 0 || pageIndex > 1)
-                {
-                    // move to ObservationQueryService?
-                    //_profilePhotosService.GetThumbnailUrl(networkObservations.Items);
-                    networkObservations.ReturnFilter = ObservationFeedFilter.Network;
-                    return Ok(networkObservations);
-                }
-            }
-
-
-            /*
-             * if public observations is requested, or
-             * if user's own or network observations is request BUT user/network has no observations
-            */
             var publicObservations = await _observationQueryService.GetPagedObservationsFeedAsync(pl => pl.SelectedPrivacyLevel == PrivacyLevel.Public, pageIndex, pageSize);
 
             if (publicObservations is null)
@@ -77,9 +33,41 @@ public class ObservationFeedController : ControllerBase
                 return StatusCode(500, $"Public observations object is null");
             }
 
-            //_profilePhotosService.GetThumbnailUrl(publicObservations.Items);
-            publicObservations.ReturnFilter = ObservationFeedFilter.Public;
             return Ok(publicObservations);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(LoggingEvents.GetListNotFound, ex, "An error occurred getting the observations feed");
+            return StatusCode(500, "An unexpected error occurred");
+        }
+    }
+
+    [HttpGet, Route("NetworkFeed")]
+    public async Task<IActionResult> GetNetworkFeedAsync(int pageIndex, int pageSize, ObservationFeedFilter filter)
+    {
+        try
+        {
+            var requestingUserAndNetwork = await _userManager.GetUserWithNetworkAsync(User.Identity.Name);
+
+            if (requestingUserAndNetwork is null)
+            {
+                _logger.LogWarning(LoggingEvents.GetItemNotFound, "Requesting user not found");
+                return StatusCode(500, $"requesting user not found");
+            }
+
+            var followingUsernamesList = UserNetworkHelpers.GetFollowingUserNames(requestingUserAndNetwork.Following);
+
+            followingUsernamesList.Add(requestingUserAndNetwork.UserName);
+
+            var networkObservations = await _observationQueryService.GetPagedObservationsFeedAsync(o => followingUsernamesList.Contains(o.ApplicationUser.UserName), pageIndex, pageSize);
+
+            if (networkObservations is null)
+            {
+                _logger.LogWarning(LoggingEvents.GetListNotFound, "Network observations list is null");
+                return StatusCode(500, $"Network observations object is null");
+            }
+
+            return Ok(networkObservations);
         }
         catch (Exception ex)
         {
@@ -93,7 +81,6 @@ public class ObservationFeedController : ControllerBase
     {
         try
         {
-
             var userObservations = await _observationQueryService.GetPagedObservationsFeedAsync(o => o.ApplicationUser.UserName == User.Identity.Name, pageIndex, pageSize);
 
             if (userObservations is null)
@@ -102,15 +89,12 @@ public class ObservationFeedController : ControllerBase
                 return StatusCode(500, $"User observations object is null");
             }
 
-            // if (userObservations.TotalItems > 0 || pageIndex > 1)
-            // {
-            // userObservations.ReturnFilter = ObservationFeedFilter.Own;
             return Ok(userObservations);
-            // }
         }
         catch (Exception ex)
         {
-
+            _logger.LogError(LoggingEvents.GetListNotFound, ex, "An error occurred getting the observations feed");
+            return StatusCode(500, "An unexpected error occurred");
         }
     }
 }
