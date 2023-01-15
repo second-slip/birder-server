98142e9b0071f1ebe21514d199642f599c44311f
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2023-01-15 17:12:44 +0000

    Unify Startup and Program into a single Program.cs

--- Birder/Program.cs
+++ Birder/Program.cs
@@ -1,41 +1,141 @@
-using Azure.Extensions.AspNetCore.Configuration.Secrets;
-using Azure.Identity;
-using Azure.Security.KeyVault.Secrets;
-using Microsoft.AspNetCore.Hosting;
+﻿using Birder.Data;
+using Birder.Data.Model;
+using Birder.Data.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
 
-namespace Birder
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-                Host.CreateDefaultBuilder(args)
-        .ConfigureAppConfiguration((context, config) =>
+var builder = WebApplication.CreateBuilder(args);
+
+// Add logging
+// builder.Logging.AddConsole //
+
+//Add Services
+builder.Services.AddMemoryCache();
+
+builder.Services.AddControllers()
+                .AddNewtonsoftJson(options =>
         {
-            if (context.HostingEnvironment.IsProduction())
-            {
-                // ToDo: rewrite for .NET 6: https://andrewlock.net/exploring-dotnet-6-part-1-looking-inside-configurationmanager-in-dotnet-6/
-                var builtConfig = config.Build();
-                var secretClient = new SecretClient(new Uri($"https://{builtConfig["KeyVaultName"]}.vault.azure.net/"), new DefaultAzureCredential());
-                config.AddAzureKeyVault(secretClient, new KeyVaultSecretManager());
-            }
-        })
-        .ConfigureWebHostDefaults(webBuilder =>
-        {
-            webBuilder.UseStartup<Startup>();
-            webBuilder.ConfigureLogging(ConfigLogging);
+            options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+            options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
         });
 
-        static void ConfigLogging(ILoggingBuilder bldr)
+var connectionStrings = builder.Configuration.GetRequiredSection("ConnectionStrings").Get<ConnectionStringsOptions>();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+              options.UseSqlServer(connectionStrings.DefaultConnection));
+
+builder.Services.AddIdentityCore<ApplicationUser>(options =>
         {
-            bldr.AddFilter(DbLoggerCategory.Database.Connection.Name, LogLevel.Information);
-        }
-    }
-}
+            options.SignIn.RequireConfirmedEmail = true;
+            options.User.RequireUniqueEmail = true;
+            // Password settings: require any eight letters or numbers
+            options.Password.RequireDigit = true;
+            options.Password.RequiredLength = 8;
+            options.Password.RequiredUniqueChars = 2;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+        })
+        .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddDefaultTokenProviders()
+        .AddSignInManager<SignInManager<ApplicationUser>>();
+
+// AutoMapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// Custom services
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IBirdRepository, BirdRepository>();
+builder.Services.AddScoped<IObservationRepository, ObservationRepository>();
+builder.Services.AddScoped<IObservationPositionRepository, ObservationPositionRepository>();
+builder.Services.AddScoped<IObservationNoteRepository, ObservationNoteRepository>();
+builder.Services.AddScoped<INetworkRepository, NetworkRepository>();
+builder.Services.AddScoped<ITweetDayRepository, TweetDayRepository>();
+
+builder.Services.AddScoped<IServerlessDatabaseService, ServerlessDatabaseService>();
+builder.Services.AddScoped<IListService, ListService>();
+builder.Services.AddScoped<IObservationQueryService, ObservationQueryService>();
+
+builder.Services.AddScoped<IObservationsAnalysisService, ObservationsAnalysisService>();
+
+builder.Services.AddScoped<IFlickrService, FlickrService>();
+builder.Services.AddScoped<IBirdThumbnailPhotoService, BirdThumbnailPhotoService>();
+
+builder.Services.AddScoped<IAuthenticationTokenService, AuthenticationTokenService>();
+
+builder.Services.AddSingleton<ISystemClockService, SystemClockService>();
+builder.Services.AddSingleton<IUrlService, UrlService>();
+
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<IXenoCantoService, XenoCantoService>();
+
+builder.Services.AddTransient<IEmailSender, EmailSender>();
+
+
+// .................
+builder.Services.Configure<AuthMessageSenderOptions>(builder.Configuration);
+builder.Services.Configure<AuthConfigOptions>(builder.Configuration.GetSection(AuthConfigOptions.AuthConfig));
+builder.Services.Configure<FlickrOptions>(builder.Configuration.GetSection(FlickrOptions.Flickr));
+
+var authConfig = builder.Configuration.GetRequiredSection("AuthConfig").Get<AuthConfigOptions>();
+
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = authConfig.BaseUrl,
+        ValidAudience = authConfig.BaseUrl,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authConfig.TokenKey))
+    };
+});
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(MyAllowSpecificOrigins,
+        builder =>
+        {
+            builder.WithOrigins(authConfig.BaseUrl);
+        });
+});
+// services.AddAzureClients(builder =>
+// {
+//     builder.AddBlobServiceClient(Configuration["ConnectionStrings:StorageConnection:blob"], preferMsi: true);
+//     builder.AddQueueServiceClient(Configuration["ConnectionStrings:StorageConnection:queue"], preferMsi: true);
+// });
+
+//builder.WebHost.ConfigureLogging(ConfigLogging); // not sure about this
+
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
--- Birder/Startup.cs
+++ Birder/Startup.cs
@@ -1,205 +1,179 @@
-using AutoMapper.EquivalencyExpression;
-using Azure.Core.Extensions;
-using Azure.Storage.Blobs;
-using Azure.Storage.Queues;
-using Birder.Data;
-using Birder.Data.Model;
-using Birder.Data.Repository;
-using Birder.Infrastructure;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Azure;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Tokens;
-using Newtonsoft.Json;
+// using AutoMapper.EquivalencyExpression;
+// using Azure.Core.Extensions;
+// using Azure.Storage.Blobs;
+// using Azure.Storage.Queues;
+// using Birder.Data;
+// using Birder.Data.Model;
+// using Birder.Data.Repository;
+// using Birder.Infrastructure;
+// using Microsoft.AspNetCore.Authentication.JwtBearer;
+// using Microsoft.AspNetCore.Builder;
+// using Microsoft.AspNetCore.Hosting;
+// using Microsoft.EntityFrameworkCore;
+// using Microsoft.Extensions.Azure;
+// using Microsoft.Extensions.Configuration;
+// using Microsoft.Extensions.DependencyInjection;
+// using Microsoft.IdentityModel.Tokens;
+// using Newtonsoft.Json;
 
-namespace Birder
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+// namespace Birder;
+// public class Startup
+// {
+//     public Startup(IConfiguration configuration)
+//     {
+//         Configuration = configuration;
+//     }
 
-        public IConfiguration Configuration { get; }
+//     public IConfiguration Configuration { get; }
 
-        readonly string _myAllowSpecificOrigins = "_myAllowSpecificOrigins";
+//     readonly string _myAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMemoryCache();
+//     public void ConfigureServices(IServiceCollection services)
+//     {
+//         services.AddMemoryCache();
 
-            services.AddControllers()
-            .AddNewtonsoftJson(options =>
-            {
-                options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
-                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-            });
+//         services.AddControllers()
+//         .AddNewtonsoftJson(options =>
+//         {
+//             options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+//             options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+//         });
 
-            // In production, the Angular files will be served from this directory
-            //services.AddSpaStaticFiles(configuration =>
-            //{
-            //    configuration.RootPath = "ClientApp/dist";
-            //});
 
-            services.AddDbContext<ApplicationDbContext>(options =>
-                  options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+//         services.AddDbContext<ApplicationDbContext>(options =>
+//               options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
 
-            services.AddIdentityCore<ApplicationUser>(options =>
-            {
-                options.SignIn.RequireConfirmedEmail = true;
-                options.User.RequireUniqueEmail = true;
-                // Password settings: require any eight letters or numbers
-                options.Password.RequireDigit = true;
-                options.Password.RequiredLength = 8;
-                options.Password.RequiredUniqueChars = 2;
-                options.Password.RequireLowercase = false;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireUppercase = false;
-            })
-            .AddEntityFrameworkStores<ApplicationDbContext>()
-            .AddDefaultTokenProviders()
-            .AddSignInManager<SignInManager<ApplicationUser>>();
+//         services.AddIdentityCore<ApplicationUser>(options =>
+//         {
+//             options.SignIn.RequireConfirmedEmail = true;
+//             options.User.RequireUniqueEmail = true;
+//             // Password settings: require any eight letters or numbers
+//             options.Password.RequireDigit = true;
+//             options.Password.RequiredLength = 8;
+//             options.Password.RequiredUniqueChars = 2;
+//             options.Password.RequireLowercase = false;
+//             options.Password.RequireNonAlphanumeric = false;
+//             options.Password.RequireUppercase = false;
+//         })
+//         .AddEntityFrameworkStores<ApplicationDbContext>()
+//         .AddDefaultTokenProviders()
+//         .AddSignInManager<SignInManager<ApplicationUser>>();
 
-            services.AddAutoMapper((serviceProvider, automapper) =>
-            {
-                automapper.AddCollectionMappers();
-                //automapper.UseEntityFrameworkCoreModel<ApplicationDbContext>(serviceProvider);
-            }, typeof(ApplicationDbContext).Assembly);
+//         services.AddAutoMapper((serviceProvider, automapper) =>
+//         {
+//             automapper.AddCollectionMappers();
+//             //automapper.UseEntityFrameworkCoreModel<ApplicationDbContext>(serviceProvider);
+//         }, typeof(ApplicationDbContext).Assembly);
 
-            services.AddScoped<IUnitOfWork, UnitOfWork>();
-            services.AddScoped<IBirdRepository, BirdRepository>();
-            services.AddScoped<IObservationRepository, ObservationRepository>();
-            services.AddScoped<IObservationPositionRepository, ObservationPositionRepository>();
-            services.AddScoped<IObservationNoteRepository, ObservationNoteRepository>();
-            services.AddScoped<INetworkRepository, NetworkRepository>();
-            services.AddScoped<ITweetDayRepository, TweetDayRepository>();
-            
-            services.AddScoped<IServerlessDatabaseService, ServerlessDatabaseService>();
-            services.AddScoped<IListService, ListService>();
-            services.AddScoped<IObservationQueryService, ObservationQueryService>();
+//         services.AddScoped<IUnitOfWork, UnitOfWork>();
+//         services.AddScoped<IBirdRepository, BirdRepository>();
+//         services.AddScoped<IObservationRepository, ObservationRepository>();
+//         services.AddScoped<IObservationPositionRepository, ObservationPositionRepository>();
+//         services.AddScoped<IObservationNoteRepository, ObservationNoteRepository>();
+//         services.AddScoped<INetworkRepository, NetworkRepository>();
+//         services.AddScoped<ITweetDayRepository, TweetDayRepository>();
 
-            services.AddScoped<IObservationsAnalysisService, ObservationsAnalysisService>();
+//         services.AddScoped<IServerlessDatabaseService, ServerlessDatabaseService>();
+//         services.AddScoped<IListService, ListService>();
+//         services.AddScoped<IObservationQueryService, ObservationQueryService>();
 
-            services.AddScoped<IFlickrService, FlickrService>();
-            services.AddScoped<IBirdThumbnailPhotoService, BirdThumbnailPhotoService>();
+//         services.AddScoped<IObservationsAnalysisService, ObservationsAnalysisService>();
 
-            services.AddScoped<IAuthenticationTokenService, AuthenticationTokenService>();
+//         services.AddScoped<IFlickrService, FlickrService>();
+//         services.AddScoped<IBirdThumbnailPhotoService, BirdThumbnailPhotoService>();
 
-            services.AddSingleton<ISystemClockService, SystemClockService>();
-            services.AddSingleton<IUrlService, UrlService>();
+//         services.AddScoped<IAuthenticationTokenService, AuthenticationTokenService>();
 
-            services.AddHttpClient();
-            services.AddScoped<IXenoCantoService, XenoCantoService>();
+//         services.AddSingleton<ISystemClockService, SystemClockService>();
+//         services.AddSingleton<IUrlService, UrlService>();
 
-            // CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
-            // CloudConfigurationManager.GetSetting("StorageConnectionString"));
+//         services.AddHttpClient();
+//         services.AddScoped<IXenoCantoService, XenoCantoService>();
 
-            //services.AddScoped<IFileClient, AzureBlobFileClient>(client =>
-            //{
-            //    var cloudConnStr = Configuration["StorageConnectionString"];
-            //    return new AzureBlobFileClient(cloudConnStr);
-            //});
+//         services.AddTransient<IEmailSender, EmailSender>();
+//         services.Configure<AuthMessageSenderOptions>(Configuration);  // < ---
 
-            services.AddTransient<IEmailSender, EmailSender>();
-            services.Configure<AuthMessageSenderOptions>(Configuration);
+//         var baseUrl = string.Concat(Configuration["Scheme"], Configuration["Domain"]);
 
-            var baseUrl = string.Concat(Configuration["Scheme"], Configuration["Domain"]);
+//         services.AddAuthentication(options =>
+//         {
+//             options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//             options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//             options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+//         })
+//         .AddJwtBearer(options =>
+//         {
+//             options.TokenValidationParameters = new TokenValidationParameters
+//             {
+//                 ValidateIssuer = true,
+//                 ValidateAudience = true,
+//                 ValidateLifetime = true,
+//                 ValidateIssuerSigningKey = true,
+//                 ValidIssuer = baseUrl,
+//                 ValidAudience = baseUrl,
+//                 IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"]))
+//             };
+//         });
 
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    ValidIssuer = baseUrl,
-                    ValidAudience = baseUrl,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"]))
-                };
-            });
+        
 
-            services.AddCors(options =>
-            {
-               options.AddPolicy(_myAllowSpecificOrigins,
-               builder =>
-               {
-                   builder.WithOrigins(baseUrl);
-               });
-            });
-            services.AddAzureClients(builder =>
-            {
-                builder.AddBlobServiceClient(Configuration["ConnectionStrings:StorageConnection:blob"], preferMsi: true);
-                builder.AddQueueServiceClient(Configuration["ConnectionStrings:StorageConnection:queue"], preferMsi: true);
-            });
-        }
+//         services.AddCors(options =>
+//         {
+//             options.AddPolicy(_myAllowSpecificOrigins,
+//                 builder =>
+//                 {
+//                     builder.WithOrigins(baseUrl);
+//                 });
+//         });
+//         services.AddAzureClients(builder =>
+//         {
+//             builder.AddBlobServiceClient(Configuration["ConnectionStrings:StorageConnection:blob"], preferMsi: true);
+//             builder.AddQueueServiceClient(Configuration["ConnectionStrings:StorageConnection:queue"], preferMsi: true);
+//         });
+//     }
 
-        // This method gets called by the runtime. 
-        // Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            app.UseStaticFiles();
-            //app.UseSpaStaticFiles();
+//     // This method gets called by the runtime. 
+//     // Use this method to configure the HTTP request pipeline.
+//     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//     {
+//         app.UseStaticFiles(); //
 
-            app.UseRouting();
+//         app.UseRouting(); //
 
-            app.UseCors(_myAllowSpecificOrigins);
-            app.UseHttpsRedirection();
-            app.UseAuthentication();
-            app.UseAuthorization();
+//         app.UseCors(_myAllowSpecificOrigins);
+//         app.UseHttpsRedirection();
+//         app.UseAuthentication();
+//         app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapDefaultControllerRoute();
-                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
-            });
-
-            //app.UseSpa(spa =>
-            //{
-            //    // To learn more about options for serving an Angular SPA from ASP.NET Core,
-            //    // see https://go.microsoft.com/fwlink/?linkid=864501
-            //    spa.Options.SourcePath = "ClientApp";
-            //    if (env.IsDevelopment())
-            //    {
-            //        spa.UseAngularCliServer(npmScript: "start");
-            //    }
-            //});
-        }
-    }
-    internal static class StartupExtensions
-    {
-        public static IAzureClientBuilder<BlobServiceClient, BlobClientOptions> AddBlobServiceClient(this AzureClientFactoryBuilder builder, string serviceUriOrConnectionString, bool preferMsi)
-        {
-            if (preferMsi && Uri.TryCreate(serviceUriOrConnectionString, UriKind.Absolute, out Uri serviceUri))
-            {
-                return builder.AddBlobServiceClient(serviceUri);
-            }
-            else
-            {
-                return builder.AddBlobServiceClient(serviceUriOrConnectionString);
-            }
-        }
-        public static IAzureClientBuilder<QueueServiceClient, QueueClientOptions> AddQueueServiceClient(this AzureClientFactoryBuilder builder, string serviceUriOrConnectionString, bool preferMsi)
-        {
-            if (preferMsi && Uri.TryCreate(serviceUriOrConnectionString, UriKind.Absolute, out Uri serviceUri))
-            {
-                return builder.AddQueueServiceClient(serviceUri);
-            }
-            else
-            {
-                return builder.AddQueueServiceClient(serviceUriOrConnectionString);
-            }
-        }
-    }
-}
+//         app.UseEndpoints(endpoints => //
+//         {
+//             endpoints.MapDefaultControllerRoute();
+//             endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+//         });
+//     }
+// }
+// internal static class StartupExtensions
+// {
+//     public static IAzureClientBuilder<BlobServiceClient, BlobClientOptions> AddBlobServiceClient(this AzureClientFactoryBuilder builder, string serviceUriOrConnectionString, bool preferMsi)
+//     {
+//         if (preferMsi && Uri.TryCreate(serviceUriOrConnectionString, UriKind.Absolute, out Uri serviceUri))
+//         {
+//             return builder.AddBlobServiceClient(serviceUri);
+//         }
+//         else
+//         {
+//             return builder.AddBlobServiceClient(serviceUriOrConnectionString);
+//         }
+//     }
+//     public static IAzureClientBuilder<QueueServiceClient, QueueClientOptions> AddQueueServiceClient(this AzureClientFactoryBuilder builder, string serviceUriOrConnectionString, bool preferMsi)
+//     {
+//         if (preferMsi && Uri.TryCreate(serviceUriOrConnectionString, UriKind.Absolute, out Uri serviceUri))
+//         {
+//             return builder.AddQueueServiceClient(serviceUri);
+//         }
+//         else
+//         {
+//             return builder.AddQueueServiceClient(serviceUriOrConnectionString);
+//         }
+//     }
+// }
