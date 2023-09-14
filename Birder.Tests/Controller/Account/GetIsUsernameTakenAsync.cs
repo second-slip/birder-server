97fe3e6f07166135f311fb5e335f750994156461
Andrew Stuart Cross <andrew-stuart-cross@outlook.com>
2023-09-14 11:50:16 +0100

    update controller action tests

--- Birder.Tests/Controller/Account/GetIsUsernameTakenAsync.cs
+++ Birder.Tests/Controller/Account/GetIsUsernameTakenAsync.cs
@@ -2,8 +2,8 @@ namespace Birder.Tests.Controller;
 
 public class GetIsUsernameTakenAsync
 {
-    [Fact]
-    public async Task GetIsUsernameAvailableAsync_ReturnsBadRequest_WhenUsernameArgumentIsNull()
+    [Theory, MemberData(nameof(InValid_Username_String_Test_Data))]
+    public async Task GetIsUsernameAvailableAsync_ReturnsBadRequest_WhenUsernameArgumentIsNull(string username)
     {
         // Arrange
         var mockUserManager = SharedFunctions.InitialiseMockUserManager();
@@ -13,18 +13,16 @@ public class GetIsUsernameTakenAsync
         var emailSender = new Mock<IEmailSender>();
         var controller = new AccountController(systemClock.Object, urlService.Object, emailSender.Object, logger.Object, mockUserManager.Object);
 
-        const string USERNAME_ARGUMENT_IS_EMPTY = "";
-
         // Act
-        var result = await controller.GetIsUsernameTakenAsync(USERNAME_ARGUMENT_IS_EMPTY);
+        var result = await controller.GetIsUsernameTakenAsync(username);
 
         // Assert
         var objectResult = Assert.IsType<StatusCodeResult>(result);
         Assert.Equal(StatusCodes.Status400BadRequest, objectResult.StatusCode);
     }
 
-    [Fact]
-    public async Task GetIsUsernameAvailableAsync_ReturnsBadRequest_WhenExceptionIsRaised()
+    [Theory, MemberData(nameof(Valid_Username_String_Test_Data))]
+    public async Task GetIsUsernameAvailableAsync_ReturnsBadRequest_WhenExceptionIsRaised(string username)
     {
         // Arrange
         var systemClock = new Mock<ISystemClockService>();
@@ -37,18 +35,16 @@ public class GetIsUsernameTakenAsync
 
         var controller = new AccountController(systemClock.Object, urlService.Object, emailSender.Object, logger.Object, mockUserManager.Object);
 
-        const string USERNAME_ARGUMENT_IS_VALID_STRING = "ANY_USER_NAME";
-
         // Act
-        var result = await controller.GetIsUsernameTakenAsync(USERNAME_ARGUMENT_IS_VALID_STRING);
+        var result = await controller.GetIsUsernameTakenAsync(username);
 
         // Assert
         var objectResult = Assert.IsType<StatusCodeResult>(result);
         Assert.Equal(StatusCodes.Status500InternalServerError, objectResult.StatusCode);
     }
 
-    [Fact]
-    public async Task GetIsUsernameAvailableAsync_ReturnsBadRequest_WhenUsernameIsTaken()
+    [Theory, MemberData(nameof(Valid_Username_String_Test_Data))]
+    public async Task GetIsUsernameAvailableAsync_ReturnsBadRequest_WhenUsernameIsTaken(string username)
     {
         // Arrange
         var systemClock = new Mock<ISystemClockService>();
@@ -70,10 +66,8 @@ public class GetIsUsernameTakenAsync
 
         var controller = new AccountController(systemClock.Object, urlService.Object, emailSender.Object, logger.Object, mockUserManager.Object);
 
-        const string USERNAME_ARGUMENT_IS_VALID_STRING = "ANY_USER_NAME";
-
         // Act
-        var result = await controller.GetIsUsernameTakenAsync(USERNAME_ARGUMENT_IS_VALID_STRING);
+        var result = await controller.GetIsUsernameTakenAsync(username);
 
         // Assert
         var objectResult = Assert.IsType<OkObjectResult>(result);
@@ -83,8 +77,8 @@ public class GetIsUsernameTakenAsync
         objectResult.Value.Should().BeEquivalentTo(expected);
     }
 
-    [Fact]
-    public async Task GetIsUsernameAvailableAsync_ReturnsOk_WhenUsernameIsAvailable()
+    [Theory, MemberData(nameof(Valid_Username_String_Test_Data))]
+    public async Task GetIsUsernameAvailableAsync_ReturnsOk_WhenUsernameIsAvailable(string username)
     {
         // Arrange
         var systemClock = new Mock<ISystemClockService>();
@@ -97,10 +91,8 @@ public class GetIsUsernameTakenAsync
 
         var controller = new AccountController(systemClock.Object, urlService.Object, emailSender.Object, logger.Object, mockUserManager.Object);
 
-        const string USERNAME_ARGUMENT_IS_VALID_STRING = "ANY_USER_NAME";
-
         // Act
-        var result = await controller.GetIsUsernameTakenAsync(USERNAME_ARGUMENT_IS_VALID_STRING);
+        var result = await controller.GetIsUsernameTakenAsync(username);
 
         // Assert
         var objectResult = Assert.IsType<OkObjectResult>(result);
@@ -108,5 +100,41 @@ public class GetIsUsernameTakenAsync
 
         var expected = new { usernameTaken = false };
         objectResult.Value.Should().BeEquivalentTo(expected);
+    }
+
+
+    public static IEnumerable<object[]> InValid_Username_String_Test_Data
+    {
+        get
+        {
+            return new[]
+            {
+                new object[] { "" },
+                new object[] { null },
+                new object[] { "@b" },
+                new object[] {  "1234" } ,
+                new object[] { "12345 " },
+                new object[] { "sdefd@b." },
+                new object[] { "sdefd@b " },
+                new object[] { "sdefdb " },
+                new object[] { "rnt7fkjjtnrgntksd5rmpZSD" },
+                new object[] { "@o.uk" },
+                new object[] { "  andrew3" }, // leading whitepace
+                new object[] { "andrew3 " }, // trailing whitespace
+            };
+        }
+    }
+
+    public static IEnumerable<object[]> Valid_Username_String_Test_Data
+    {
+        get
+        {
+            return new[]
+            {
+                new object[] { "andrew1" },
+                new object[] { "andrew" },
+                new object[] {  "jimmy77689rt" } ,
+            };
+        }
     }
 }
