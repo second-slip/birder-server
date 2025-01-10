369f5b354ffe2c61b835fb2e8ddc2312c6bef85a
Andrew Cross <andrew-stuart-cross@outlook.com>
2025-01-10 20:46:49 +0000

    change min api  response return type

--- Birder.Tests/MinApiEndpointTests/BirdListDdlEndpointTests.cs
+++ Birder.Tests/MinApiEndpointTests/BirdListDdlEndpointTests.cs
@@ -17,7 +17,7 @@ public class GetBirdsDdlAsyncTests
 
         var result = await BirdEndpoints.GetBirdsAsync(mock.Object, It.IsAny<int>(), It.IsAny<int>(), It.IsAny<BirderStatus>());
 
-        Assert.IsType<Ok<BirdsListDto>>(result);
+        Assert.IsType<Ok<BirdsListDto>>(result.Result);
     }
 
     [Fact]
@@ -31,7 +31,7 @@ public class GetBirdsDdlAsyncTests
 
         var result = await BirdEndpoints.GetBirdsAsync(service.Object, It.IsAny<int>(), It.IsAny<int>(), It.IsAny<BirderStatus>());
 
-        Assert.IsType<NotFound>(result);
+        Assert.IsType<NotFound>(result.Result);
     }
 
     [Fact]
@@ -42,7 +42,7 @@ public class GetBirdsDdlAsyncTests
 
         var result = await BirdEndpoints.GetBirdsAsync(service, It.IsAny<int>(), It.IsAny<int>(), It.IsAny<BirderStatus>());
 
-        Assert.IsType<BadRequest>(result);
+        Assert.IsType<BadRequest>(result.Result);
     }
 
 
@@ -77,7 +77,9 @@ public class GetBirdsDdlAsyncTests
 
         var result = await BirdEndpoints.GetBirdAsync(mock.Object, requestedId);
 
-        Assert.IsType<Ok<BirdDetailDto>>(result);
+        // Assert
+        var okResult = Assert.IsType<Ok<BirdDetailDto>>(result.Result);
+        Assert.Equal(requestedId, okResult.Value.BirdId);
     }
 
     [Fact]
@@ -93,20 +95,20 @@ public class GetBirdsDdlAsyncTests
 
         var result = await BirdEndpoints.GetBirdAsync(service.Object, requestedId);
 
-        Assert.IsType<NotFound>(result);
+        Assert.IsType<NotFound>(result.Result);
     }
 
     [Fact]
     public async Task GetBirdAsync_Returns_BadRequest_When_Service_Is_Null()
     {
         // Arrange
-        IBirdDataService service = null; // new Mock<IBirdDataService>(null);
+        IBirdDataService service = null;
 
         var requestedId = 1009;
 
         var result = await BirdEndpoints.GetBirdAsync(service, requestedId);
 
-        Assert.IsType<BadRequest>(result);
+        Assert.IsType<BadRequest>(result.Result);
     }
 
     [Fact]
@@ -123,7 +125,7 @@ public class GetBirdsDdlAsyncTests
         var result = await BirdEndpoints.GetBirdsDdlAsync(mock.Object);
 
         // Assert
-        Assert.IsType<NotFound>(result);
+        Assert.IsType<NotFound>(result.Result);
         // var notFoundResult = (NotFound)result.Result;
         // Assert.NotNull(notFoundResult);
 
@@ -176,49 +178,29 @@ public class GetBirdsDdlAsyncTests
         var result = await BirdEndpoints.GetBirdsDdlAsync(mock.Object);
 
         // Assert
-        Assert.IsType<Ok<IReadOnlyList<BirdSummaryDto>>>(result);
+        var okResult = Assert.IsType<Ok<IReadOnlyList<BirdSummaryDto>>>(result.Result);
 
-        // var okResult = (IReadOnlyList<BirdSummaryDto>)result.;
-        // // Assert.NotNull(okResult.Value);
-        // // Assert.NotEmpty(okResult.Value);
-        // Assert.Collection(okResult, bird1 =>
-        // {
-        //     Assert.Equal(1, bird1.BirdId);
-        //     Assert.Equal("Test species 1", bird1.EnglishName);
-        // }, bird2 =>
-        // {
-        //     Assert.Equal(2, bird2.BirdId);
-        //     Assert.Equal("Test species 2", bird2.EnglishName);
-        // });
-
-
+        Assert.Collection(okResult.Value, bird1 =>
+        {
+            Assert.Equal(1, bird1.BirdId);
+            Assert.Equal("Test species 1", bird1.EnglishName);
+        }, bird2 =>
+        {
+            Assert.Equal(2, bird2.BirdId);
+            Assert.Equal("Test species 2", bird2.EnglishName);
+        });
     }
 
-    // [Fact]
-    // public async Task GetBirdsDdlAsync_Returns_500_On_Exception()
-    // {
-    //     // Arrange
-    //     Mock<ILogger<BirdEndpoints>> loggerMock = new();
-    //     var mock = new Mock<ICachedBirdsDdlService>();
+    [Fact]
+    public async Task GetBirdsDdlAsync_Returns_BadRequest_When_Service_Is_Null()
+    {
+        // Arrange
+        ICachedBirdsDdlService service = null;
 
-    //     mock.Setup(m => m.GetAll())
-    //         .Throws(new InvalidOperationException());
+        // Act
+        var result = await BirdEndpoints.GetBirdsDdlAsync(service);
 
-    //     // Act
-    //     var result = await BirdEndpoints.GetBirdsDdlAsync(mock.Object, loggerMock.Object);
-
-    //     // Assert
-    //     Assert.IsType<Results<Ok<IReadOnlyList<BirdSummaryDto>>, NotFound, StatusCodeHttpResult>>(result);
-    //     var internalServerError = (StatusCodeHttpResult)result.Result;
-    //     Assert.Equal(StatusCodes.Status500InternalServerError, internalServerError.StatusCode);
-    //     Assert.NotNull(internalServerError);
-
-    //     loggerMock.Verify(x => x.Log(
-    //         It.IsAny<LogLevel>(),
-    //         It.IsAny<EventId>(),
-    //         It.Is<It.IsAnyType>((v, t) => true),
-    //         It.IsAny<Exception>(),
-    //         It.IsAny<Func<It.IsAnyType, Exception, string>>()),
-    //         Times.Once);
-    // }
+        // Assert
+        Assert.IsType<BadRequest>(result.Result);
+    }
 }
--- Birder/MinApiEndpoints/BirdEndpoints.cs
+++ Birder/MinApiEndpoints/BirdEndpoints.cs
@@ -1,34 +1,35 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
 namespace Birder.MinApiEndpoints;
 
 public class BirdEndpoints
 {
     // to do integration tests on error response
-    public static async Task<IResult> GetBirdsDdlAsync(ICachedBirdsDdlService service)//, ILogger<BirdEndpoints> logger)
+    public static async Task<Results<Ok<IReadOnlyList<BirdSummaryDto>>, NotFound, BadRequest>> GetBirdsDdlAsync(ICachedBirdsDdlService service)//, ILogger<BirdEndpoints> logger)
     {
-        var model = await service.GetAll();
+        if (service is null) return TypedResults.BadRequest();
 
+        var model = await service.GetAll();
         return model.Any()
             ? TypedResults.Ok(model)
             : TypedResults.NotFound();
     }
 
-    public static async Task<IResult> GetBirdAsync(IBirdDataService service, int id)
+    public static async Task<Results<Ok<BirdDetailDto>, NotFound, BadRequest>> GetBirdAsync(IBirdDataService service, int id)
     {
         if (service is null) return TypedResults.BadRequest();
 
         var model = await service.GetBirdAsync(id);
-
         if (model is null) return TypedResults.NotFound();
 
         return TypedResults.Ok(model);
     }
 
-    public static async Task<IResult> GetBirdsAsync(IBirdDataService service, int pageIndex, int pageSize, BirderStatus speciesFilter)
+    public static async Task<Results<Ok<BirdsListDto>, NotFound, BadRequest>> GetBirdsAsync(IBirdDataService service, int pageIndex, int pageSize, BirderStatus speciesFilter)
     {
         if (service is null) return TypedResults.BadRequest();
 
         var model = await service.GetBirdsAsync(pageIndex, pageSize, speciesFilter);
-
         if (model is null) return TypedResults.NotFound();
 
         return TypedResults.Ok(model);
